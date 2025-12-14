5ee7cb5de57d558beb9794cf876b890afbe6f17f
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2025-12-13 22:36:06 -0300

    Update update-date.js

--- assets/js/update-date.js
+++ assets/js/update-date.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const lastUpdateElements = document.querySelectorAll('.last-update');
     
     // Define a data de atualização aqui:
-    const dataAtualizacao = "09 de dezembro de 2025";
+    const dataAtualizacao = "13 de dezembro de 2025";
 
     lastUpdateElements.forEach(el => {
         el.textContent = `Última atualização em: ${dataAtualizacao}`;
