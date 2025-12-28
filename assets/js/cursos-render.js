bf280dba58b4c9b1b28f7b0953aba2d8b37707eb
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2025-12-28 14:51:36 -0300

    Update cursos-render.js

--- assets/js/cursos-render.js
+++ assets/js/cursos-render.js
@@ -14,18 +14,9 @@ function renderListaCursos(cursos) {
 
     item.innerHTML = `
       <span class="course-name">${curso.curso}</span>
-      <span class="course-date">${formatarData(curso.conclusao)}</span>
+      <span class="course-date">${curso.conclusaoExtenso}</span>
     `;
 
     container.appendChild(item);
-  });
-}
-
-function formatarData(dataISO) {
-  const data = new Date(dataISO);
-  return data.toLocaleDateString("pt-BR", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric"
   });
 }
