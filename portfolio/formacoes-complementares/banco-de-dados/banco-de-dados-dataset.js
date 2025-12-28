3c0f67ed389afd86a4b3699eafb491881a490c21
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2025-12-28 13:30:45 -0300

    Create banco-de-dados-dataset.js

---
+++ portfolio/formacoes-complementares/banco-de-dados/banco-de-dados-dataset.js
@@ -0,0 +1,388 @@
+const cursos = [
+  {
+    id: 1,
+
+    instituicao: "Fundação Bradesco",
+    curso: "Administrando Banco de Dados",
+    cargaHoraria: "15 h",
+
+    dataConclusao: "05 de outubro de 2024",
+
+    codigo: "7509592B-143A-4419-8F0A-AC1CCE0B26E2",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2024.10.05 · Fundação Bradesco · Administrando Banco de Dados.png",
+
+    verificacao: {
+      url: "https://www.ev.org.br/validar-certificado/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 2,
+
+    instituicao: "Cursa",
+    curso: "MySQL",
+    cargaHoraria: "7 h 51 min",
+
+    dataConclusao: "17 de outubro de 2024",
+
+    codigo: "u5153989",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2024.10.17 · Cursa · MySQL.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 3,
+
+    instituicao: "Escola de Inteligência Artificial",
+    curso: "Introdução ao MongoDB: Fácil e Simples!",
+    cargaHoraria: "5 h",
+
+    dataConclusao: "17 de outubro de 2024",
+
+    codigo: "x60COYRo",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2024.10.17 · Escola de Inteligência Artificial · Introdução ao MongoDB · Fácil e Simples!.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 4,
+
+    instituicao: "Escola de Inteligência Artificial",
+    curso: "Introdução a Bancos de Dados e Linguagem SQL",
+    cargaHoraria: "7 h",
+
+    dataConclusao: "21 de outubro de 2024",
+
+    codigo: "qEjIxm7B",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2024.10.21 · Escola de Inteligência Artificial · Introdução a Bancos de Dados e Linguagem SQL.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 5,
+
+    instituicao: "Cursa",
+    curso: "Oracle Database do Zero ao Avançado",
+    cargaHoraria: "9 h",
+
+    dataConclusao: "04 de novembro de 2024",
+
+    codigo: "093a041fa3",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2024.11.04 · Cursa · Oracle Database do Zero ao Avançado.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/093a041fa3",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 6,
+
+    instituicao: "Cursa",
+    curso: "MySQL",
+    cargaHoraria: "14 h",
+
+    dataConclusao: "09 de novembro de 2024",
+
+    codigo: "279c8cdffe",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2024.11.09 · Cursa · MySQL.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/279c8cdffe",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 7,
+
+    instituicao: "Unieducar",
+    curso: "PHP/MySQL",
+    cargaHoraria: "4 h",
+
+    dataConclusao: "09 de novembro de 2024",
+
+    codigo: "202411093657557.1731146220",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2024.11.09 · Unieducar · PHP·MySQL.png",
+
+    verificacao: {
+      url: "https://unieducar.org.br/consultar-certificado?numero_certificado=202411093657557.1731146220",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 8,
+
+    instituicao: "Cursa",
+    curso: "Banco de Dados com PostgreSQL",
+    cargaHoraria: "7 h",
+
+    dataConclusao: "11 de novembro de 2024",
+
+    codigo: "395ef1823a",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2024.11.11 · Cursa · Banco de Dados com PostgreSQL.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/395ef1823a",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 9,
+
+    instituicao: "Instituto Federal Espírito Santo",
+    curso: "Introdução a NoSQL com MongoDB",
+    cargaHoraria: "60 h",
+
+    dataConclusao: "21 de março de 2025",
+
+    codigo: "67dda60b-61b8-401e-aee5-25f0ac1a2c09",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.03.21 · Instituto Federal Espírito Santo · Introdução a NoSQL com MongoDB.png",
+
+    verificacao: {
+      url: "https://mooc.cefor.ifes.edu.br/moodle/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 10,
+
+    instituicao: "Instituto Federal Sul-Rio-Grandense",
+    curso: "Banco de Dados",
+    cargaHoraria: "40 h",
+
+    dataConclusao: "29 de março de 2025",
+
+    codigo: "67e81c1a-8a6c-4918-b74a-69430a00c840",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.03.29 · Instituto Federal Sul-Rio-Grandense · Banco de Dados.png",
+
+    verificacao: {
+      url: "https://mundi.ifsul.edu.br/ava/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 11,
+
+    instituicao: "Unieducar",
+    curso: "PHP/MySQL",
+    cargaHoraria: "4 h",
+
+    dataConclusao: "05 de abril de 2024",
+
+    codigo: "202504053720346.1743884228",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.04.05 · Unieducar · PHP·MySQL.png",
+
+    verificacao: {
+      url: "https://unieducar.org.br/consultar-certificado?numero_certificado=202504053720346.1743884228",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 12,
+
+    instituicao: "Xperiun | Data Analytics",
+    curso: "SQL",
+    cargaHoraria: "2 h",
+
+    dataConclusao: "06 de abril de 2024",
+
+    codigo: "Inaplicável",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.04.06 · Xperiun · Data Analytics · Minicurso de SQL.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 13,
+
+    instituicao: "Cursa",
+    curso: "Estrutura de Dados",
+    cargaHoraria: "27 h",
+
+    dataConclusao: "14 de agosto de 2025",
+
+    codigo: "ab7e82f739",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.08.14 · Cursa · Estrutura de Dados.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/ab7e82f739",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 14,
+
+    instituicao: "Aprenda Mais",
+    curso: "Banco de Dados 1: Fundamentos",
+    cargaHoraria: "20 h",
+
+    dataConclusao: "29 de setembro de 2025",
+
+    codigo: "68daf634-daa0-4b41-80b2-0d20ac1f0292",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.09.29 · Aprenda Mais · Banco de Dados 1 (Fundamentos).png",
+
+    verificacao: {
+      url: "https://aprendamais.mec.gov.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 15,
+
+    instituicao: "Instituto Federal Rio Grande do Sul",
+    curso: "Banco de Dados 1: Fundamentos",
+    cargaHoraria: "20 h",
+
+    dataConclusao: "10 de outubro de 2025",
+
+    codigo: "68e97ba3-baa4-46e9-bf6a-30580aa8010c",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.10.10 · Instituto Federal Rio Grande do Sul · Banco de Dados 1 (Fundamentos).png",
+
+    verificacao: {
+      url: "https://moodle.ifrs.edu.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 16,
+
+    instituicao: "Cursa",
+    curso: "Firebird (SQL)",
+    cargaHoraria: "12 h",
+
+    dataConclusao: "11 de outubro de 2025",
+
+    codigo: "2d594c2dc0",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.10.11 · Cursa · Firebird (SQL).png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/2d594c2dc0",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 17,
+
+    instituicao: "Instituto Federal Rio Grande do Sul",
+    curso: "Banco de Dados: Oracle PL/SQL",
+    cargaHoraria: "20 h",
+
+    dataConclusao: "12 de outubro de 2025",
+
+    codigo: "68eb21ed-c064-4bcc-b743-35e40aa80e10",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.10.12 · Instituto Federal Rio Grande do Sul · Banco de Dados (Oracle PL·SQL).png",
+
+    verificacao: {
+      url: "https://moodle.ifrs.edu.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 18,
+
+    instituicao: "Cursa",
+    curso: "PostgreSQL",
+    cargaHoraria: "11 h",
+
+    dataConclusao: "12 de outubro de 2025",
+
+    codigo: "e22abddbae",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.10.12 · Cursa · PostgreSQL.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/e22abddbae",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 19,
+
+    instituicao: "Cursa",
+    curso: "Modelagem de Banco de Dados Relacionais",
+    cargaHoraria: "10 h",
+
+    dataConclusao: "13 de outubro de 2025",
+
+    codigo: "55d1d706c2",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.10.13 · Cursa · Modelagem de Banco de Dados Relacionais.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/55d1d706c2",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 20,
+
+    instituicao: "Cursa",
+    curso: "SQL Server",
+    cargaHoraria: "9 h",
+
+    dataConclusao: "13 de outubro de 2025",
+
+    codigo: "314a9531d2",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.10.13 · Cursa · SQL Server.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/314a9531d2",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 21,
+
+    instituicao: "Instituto Federal Rio Grande do Sul",
+    curso: "Administrador de Banco de Dados",
+    cargaHoraria: "200 h",
+
+    dataConclusao: "21 de novembro de 2025",
+
+    codigo: "6920eba3-52ac-4d36-963c-cbe00aa81311",
+
+    thumb: "/portfolio/formacoes-complementares/banco-de-dados/certificados/2025.11.21 · Instituto Federal Rio Grande do Sul · Administrador de Banco de Dados.png",
+
+    verificacao: {
+      url: "https://moodle.ifrs.edu.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
