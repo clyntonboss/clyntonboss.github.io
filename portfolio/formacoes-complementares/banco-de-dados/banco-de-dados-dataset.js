cca15cb37e7530b921ad702c57cf7f4109743822
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2025-12-28 14:46:08 -0300

    Update banco-de-dados-dataset.js

--- portfolio/formacoes-complementares/banco-de-dados/banco-de-dados-dataset.js
+++ portfolio/formacoes-complementares/banco-de-dados/banco-de-dados-dataset.js
@@ -6,6 +6,8 @@ const cursosBancoDeDados = [
     curso: "Administrando Banco de Dados",
     cargaHoraria: "15 h",
 
+    conclusaoISO: "2024-10-05",
+
     dataConclusao: "05 de outubro de 2024",
 
     codigo: "7509592B-143A-4419-8F0A-AC1CCE0B26E2",
@@ -25,6 +27,8 @@ const cursosBancoDeDados = [
     curso: "MySQL",
     cargaHoraria: "7 h 51 min",
 
+    conclusaoISO: "2024-10-17",
+
     dataConclusao: "17 de outubro de 2024",
 
     codigo: "u5153989",
@@ -40,6 +44,8 @@ const cursosBancoDeDados = [
     instituicao: "Escola de Inteligência Artificial",
     curso: "Introdução ao MongoDB: Fácil e Simples!",
     cargaHoraria: "5 h",
+
+    conclusaoISO: "2024-10-17",
 
     dataConclusao: "17 de outubro de 2024",
 
@@ -57,6 +63,8 @@ const cursosBancoDeDados = [
     curso: "Introdução a Bancos de Dados e Linguagem SQL",
     cargaHoraria: "7 h",
 
+    conclusaoISO: "2024-10-21",
+
     dataConclusao: "21 de outubro de 2024",
 
     codigo: "qEjIxm7B",
@@ -72,6 +80,8 @@ const cursosBancoDeDados = [
     instituicao: "Cursa",
     curso: "Oracle Database do Zero ao Avançado",
     cargaHoraria: "9 h",
+
+    conclusaoISO: "2024-11-04",
 
     dataConclusao: "04 de novembro de 2024",
 
@@ -92,6 +102,8 @@ const cursosBancoDeDados = [
     curso: "MySQL",
     cargaHoraria: "14 h",
 
+    conclusaoISO: "2024-11-09",
+
     dataConclusao: "09 de novembro de 2024",
 
     codigo: "279c8cdffe",
@@ -110,6 +122,8 @@ const cursosBancoDeDados = [
     instituicao: "Unieducar",
     curso: "PHP/MySQL",
     cargaHoraria: "4 h",
+
+    conclusaoISO: "2024-11-09",
 
     dataConclusao: "09 de novembro de 2024",
 
@@ -130,6 +144,8 @@ const cursosBancoDeDados = [
     curso: "Banco de Dados com PostgreSQL",
     cargaHoraria: "7 h",
 
+    conclusaoISO: "2024-11-11",
+
     dataConclusao: "11 de novembro de 2024",
 
     codigo: "395ef1823a",
@@ -148,6 +164,8 @@ const cursosBancoDeDados = [
     instituicao: "Instituto Federal Espírito Santo",
     curso: "Introdução a NoSQL com MongoDB",
     cargaHoraria: "60 h",
+
+    conclusaoISO: "2025-03-21",
 
     dataConclusao: "21 de março de 2025",
 
@@ -168,6 +186,8 @@ const cursosBancoDeDados = [
     curso: "Banco de Dados",
     cargaHoraria: "40 h",
 
+    conclusaoISO: "2025-03-29",
+
     dataConclusao: "29 de março de 2025",
 
     codigo: "67e81c1a-8a6c-4918-b74a-69430a00c840",
@@ -186,6 +206,8 @@ const cursosBancoDeDados = [
     instituicao: "Unieducar",
     curso: "PHP/MySQL",
     cargaHoraria: "4 h",
+
+    conclusaoISO: "2025-04-05",
 
     dataConclusao: "05 de abril de 2024",
 
@@ -206,7 +228,9 @@ const cursosBancoDeDados = [
     curso: "SQL",
     cargaHoraria: "2 h",
 
-    dataConclusao: "06 de abril de 2024",
+    conclusaoISO: "2025-04-06",
+
+    dataConclusao: "06 de abril de 2025",
 
     codigo: "Inaplicável",
 
@@ -221,6 +245,8 @@ const cursosBancoDeDados = [
     instituicao: "Cursa",
     curso: "Estrutura de Dados",
     cargaHoraria: "27 h",
+
+    conclusaoISO: "2025-08-14",
 
     dataConclusao: "14 de agosto de 2025",
 
@@ -241,6 +267,8 @@ const cursosBancoDeDados = [
     curso: "Banco de Dados 1: Fundamentos",
     cargaHoraria: "20 h",
 
+    conclusaoISO: "2025-09-29",
+
     dataConclusao: "29 de setembro de 2025",
 
     codigo: "68daf634-daa0-4b41-80b2-0d20ac1f0292",
@@ -259,6 +287,8 @@ const cursosBancoDeDados = [
     instituicao: "Instituto Federal Rio Grande do Sul",
     curso: "Banco de Dados 1: Fundamentos",
     cargaHoraria: "20 h",
+
+    conclusaoISO: "2025-10-10",
 
     dataConclusao: "10 de outubro de 2025",
 
@@ -279,6 +309,8 @@ const cursosBancoDeDados = [
     curso: "Firebird (SQL)",
     cargaHoraria: "12 h",
 
+    conclusaoISO: "2025-10-11",
+
     dataConclusao: "11 de outubro de 2025",
 
     codigo: "2d594c2dc0",
@@ -297,6 +329,8 @@ const cursosBancoDeDados = [
     instituicao: "Instituto Federal Rio Grande do Sul",
     curso: "Banco de Dados: Oracle PL/SQL",
     cargaHoraria: "20 h",
+
+    conclusaoISO: "2025-10-12",
 
     dataConclusao: "12 de outubro de 2025",
 
@@ -317,6 +351,8 @@ const cursosBancoDeDados = [
     curso: "PostgreSQL",
     cargaHoraria: "11 h",
 
+    conclusaoISO: "2025-10-12",
+
     dataConclusao: "12 de outubro de 2025",
 
     codigo: "e22abddbae",
@@ -335,6 +371,8 @@ const cursosBancoDeDados = [
     instituicao: "Cursa",
     curso: "Modelagem de Banco de Dados Relacionais",
     cargaHoraria: "10 h",
+
+    conclusaoISO: "2025-10-13",
 
     dataConclusao: "13 de outubro de 2025",
 
@@ -355,6 +393,8 @@ const cursosBancoDeDados = [
     curso: "SQL Server",
     cargaHoraria: "9 h",
 
+    conclusaoISO: "2025-10-13",
+
     dataConclusao: "13 de outubro de 2025",
 
     codigo: "314a9531d2",
@@ -373,6 +413,8 @@ const cursosBancoDeDados = [
     instituicao: "Instituto Federal Rio Grande do Sul",
     curso: "Administrador de Banco de Dados",
     cargaHoraria: "200 h",
+
+    conclusaoISO: "2025-11-21",
 
     dataConclusao: "21 de novembro de 2025",
 
