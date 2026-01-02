acd29c5ce9d93d40a58124eda0cf3d6f87f14c11
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-02 09:29:48 -0300

    Create data-storytelling-dataset.js

---
+++ portfolio/formacoes-complementares/data-storytelling/data-storytelling-dataset.js
@@ -0,0 +1,21 @@
+const cursosBancoDeDados = [
+  {
+    id: 1,
+
+    instituicao: "Simplifica Treinamentos",
+    curso: "MasterClass PowerPoint",
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-08-28",
+
+    dataConclusao: "28 de agosto de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/data-storytelling/certificados/2025.08.28 · Simplifica Treinamentos · MasterClass PowerPoint.png",
+
+    verificacao: null
+  }
+];
