03a89ec6b721025e075e0433c83ff2839a6560b9
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-04 19:59:16 -0300

    Update formacoes-complementares-categorias.js

--- portfolio/formacoes-complementares/formacoes-complementares-categorias.js
+++ portfolio/formacoes-complementares/formacoes-complementares-categorias.js
@@ -1,4 +1,4 @@
-const categorias = {
+const categoriasFormacoesComplementares = {
   bancoDados: {
     title: "Formações Complementares | Banco de Dados",
     dataset: "/portfolio/formacoes-complementares/banco-de-dados/banco-de-dados-dataset.js",
