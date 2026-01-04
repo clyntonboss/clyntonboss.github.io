d3764094f43b896ba1d0105756065b442fc36b59
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-04 18:34:05 -0300

    Create formacoes-complementares-categorias.js

---
+++ portfolio/formacoes-complementares/formacoes-complementares-categorias.js
@@ -0,0 +1,21 @@
+const categoriasConfig = {
+  "banco-de-dados": {
+    title: "Formações Complementares | Banco de Dados",
+    icon: "/assets/icons/courses-icon.png",
+    microcopy: `
+      Esta seção apresenta formações complementares na área de Banco de Dados...
+    `,
+    curadoria: `
+      As formações em Banco de Dados consolidaram competências essenciais...
+    `,
+    dataset: "/portfolio/formacoes-complementares/banco-de-dados/banco-de-dados-dataset.js"
+  },
+
+  "ciencia-de-dados": {
+    title: "Formações Complementares | Ciência de Dados",
+    icon: "/assets/icons/data-science-icon.png",
+    microcopy: `...`,
+    curadoria: `...`,
+    dataset: "/portfolio/formacoes-complementares/ciencia-de-dados/dataset.js"
+  }
+};
