9253869ddde480e540c23a5b1b295bf9ab35f5e5
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-06 13:24:41 -0300

    Update formacoes-complementares-categorias.js

--- portfolio/formacoes-complementares/formacoes-complementares-categorias.js
+++ portfolio/formacoes-complementares/formacoes-complementares-categorias.js
@@ -50,4 +50,56 @@ const categoriasFormacoesComplementares = {
                                  O domínio desses conceitos sustenta atividades de análise de dados, inteligência de negócios e desenvolvimento de sistemas, garantindo integridade, desempenho e confiabilidade da informação ao longo dos processos.</div>
     `
   },
+
+  dataStorytelling: {
+    title: "Formações Complementares | Data Storytelling",
+    dataset: "/portfolio/formacoes-complementares/data-storytelling/data-storytelling-dataset.js",
+    showViewModes: true,
+    content: `
+      <div class="tech-subtitle">Esta seção reúne formações complementares na área de Data Storytelling, organizadas por conclusão cronológica.<br>
+                                 Os cursos apresentados abordam técnicas de comunicação de dados, construção de narrativas visuais e tradução de análises em mensagens claras e orientadas à tomada de decisão.</div>
+      
+      <hr class="decorative-line-cert">
+      
+      <div id="course-block" class="course-block hidden">
+          <div class="course-block-content"></div>
+      
+          <div class="course-block-nav">
+          
+            <span id="first-course" class="course-nav-action" role="button">
+              ⏮ Primeiro
+            </span>
+          
+            <span id="prev-course" class="course-nav-action" role="button">
+              ◀ Anterior
+            </span>
+          
+            <span id="course-indicator" class="course-nav-indicator">
+              1 / 21
+            </span>
+          
+            <span id="next-course" class="course-nav-action" role="button">
+              Próximo ▶
+            </span>
+          
+            <span id="last-course" class="course-nav-action" role="button">
+              Último ⏭
+            </span>
+          
+          </div>
+      
+      </div>
+      
+      <div id="courses-flow" class="courses-flow hidden"></div>
+      
+      <div id="courses-container" class="courses-list hidden"></div>
+      
+      <div id="courses-grid" class="courses-grid hidden"></div>
+      
+      <hr class="decorative-line-cert">
+      
+      <div class="tech-subtitle">As formações em Data Storytelling fortaleceram a capacidade de transformar análises e indicadores em narrativas compreensíveis e relevantes para diferentes públicos.<br>
+                                 Esse conjunto de competências sustenta a criação de visualizações eficazes, apresentações executivas e painéis analíticos orientados à clareza, contexto e impacto na tomada de decisão.</div>
+    `
+  },
 };
