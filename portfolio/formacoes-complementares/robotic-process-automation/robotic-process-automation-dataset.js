cfa78b9ddb42f6e90171753040675912da61ade0
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-10 19:19:10 -0300

    Create robotic-process-automation-dataset.js

---
+++ portfolio/formacoes-complementares/robotic-process-automation/robotic-process-automation-dataset.js
@@ -0,0 +1,23 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "RPA Solutions",
+ 
+    curso: "Robotic Process Automation",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-02-17",
+
+    dataConclusao: "17 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/robotic-process-automation/certificados/2024.02.17 · RPA Solutions · Robotic Process Automation.png",
+
+    verificacao: null
+  }
+];
