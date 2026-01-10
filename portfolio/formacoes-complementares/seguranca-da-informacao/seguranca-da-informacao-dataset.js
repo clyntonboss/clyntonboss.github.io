eb362b08ba85083040510b3da3e8a4f1721cca33
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-10 18:59:45 -0300

    Create seguranca-da-informacao-dataset.js

---
+++ portfolio/formacoes-complementares/seguranca-da-informacao/seguranca-da-informacao-dataset.js
@@ -0,0 +1,170 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "Fundação Getúlio Vargas",
+ 
+    curso: "Cibersegurança: Anatomia Clássica de um Ciberataque",
+    
+    cargaHoraria: "9 h",
+
+    conclusaoISO: "2024-02-21",
+
+    dataConclusao: "21 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/seguranca-da-informacao/certificados/2024.02.21 · Fundação Getúlio Vargas · Cibersegurança (Anatomia Clássica de um Ciberataque).png",
+
+    verificacao: null
+  },
+
+  {
+    id: 2,
+
+    instituicao: "HP Foundation",
+ 
+    curso: "Introdução aos Conhecimentos em Cibersegurança",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2024-10-12",
+
+    dataConclusao: "12 de outubro de 2024",
+
+    codigo: "4e8550dc-8ce5-484a-9308-69a48b209b41",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/seguranca-da-informacao/certificados/2024.10.12 · HP Foundation · Introdução aos Conhecimentos em Cibersegurança.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 3,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Cybersecurity",
+    
+    cargaHoraria: "120 h",
+
+    conclusaoISO: "2025-03-11",
+
+    dataConclusao: "11 de março de 2025",
+
+    codigo: "e4d8ee6b458e7183b8c89e6df3c3503d",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/seguranca-da-informacao/certificados/2025.03.11 · FIAP - Centro Universitário · Cybersecurity.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=e4d8ee6b458e7183b8c89e6df3c3503d&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 4,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Blockchain · Advanced",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-03-13",
+
+    dataConclusao: "13 de março de 2025",
+
+    codigo: "0aaf2ddd8b649cb262a40572387006c2",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/seguranca-da-informacao/certificados/2025.03.13 · FIAP - Centro Universitário · Blockchain · Advanced.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=0aaf2ddd8b649cb262a40572387006c2&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 5,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Blockchain",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-04-10",
+
+    dataConclusao: "10 de abril de 2025",
+
+    codigo: "f5867705a913ce722ef2784edf3ad23c",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/seguranca-da-informacao/certificados/2025.04.10 · FIAP - Centro Universitário · Blockchain.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=f5867705a913ce722ef2784edf3ad23c&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 6,
+
+    instituicao: "Instituto Federal Minas Gerais",
+ 
+    curso: "Fundamentos de Lógica, Teoria dos Números e Criptografia",
+    
+    cargaHoraria: "30 h",
+
+    conclusaoISO: "2025-05-27",
+
+    dataConclusao: "27 de maio de 2025",
+
+    codigo: "6836686f-03c4-46c1-9626-ad6ac883362e",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/seguranca-da-informacao/certificados/2025.05.27 · Instituto Federal Minas Gerais · Fundamentos de Lógica, Teoria dos Números e Criptografia.png",
+
+    verificacao: {
+      url: "https://mais.ifmg.edu.br/maisifmg/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 7,
+
+    instituicao: "Pontifícia Universidade Católica do Rio Grande do Sul",
+ 
+    curso: "Compliance e Proteção de Dados",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2025-11-25",
+
+    dataConclusao: "25 de novembro de 2025",
+
+    codigo: "237810-2954-1",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/seguranca-da-informacao/certificados/2025.12.25 · Pontifícia Universidade Católica do Rio Grande do Sul · Compliance e Proteção de Dados.png",
+
+    verificacao: {
+      url: "https://webappcl.pucrs.br/inscricoes/verificacao",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
