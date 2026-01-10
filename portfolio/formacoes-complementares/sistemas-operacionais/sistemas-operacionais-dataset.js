23c086782209386d3f24caf08c86583086c34af1
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-10 13:29:20 -0300

    Create sistemas-operacionais-dataset.js

---
+++ portfolio/formacoes-complementares/sistemas-operacionais/sistemas-operacionais-dataset.js
@@ -0,0 +1,51 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Linux Fundamentos",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-03-10",
+
+    dataConclusao: "10 de março de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.03.10 · FIAP Centro Universitário · Linux Fundamentos.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=b680d2fffda89a1e7ad608953a3f7fac&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 2,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Android",
+    
+    cargaHoraria: "80 h",
+
+    conclusaoISO: "2025-04-15",
+
+    dataConclusao: "15 de abril de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.04.15 · FIAP Centro Universitário · Android.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=ff03a7806ef30659780b9ff061b98936&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
