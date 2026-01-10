eca662b8126290a2d93bad02b883ca56a410d693
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-10 12:37:14 -0300

    Create big-data-dataset.js

---
+++ portfolio/formacoes-complementares/big-data/big-data-dataset.js
@@ -0,0 +1,51 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "Cursa",
+ 
+    curso: "Google BigQuery",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2024-10-23",
+
+    dataConclusao: "23 de outubro de 2024",
+
+    codigo: "ff39b8bbf4",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2024.10.23 · Cursa · Google BigQuery.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/ff39b8bbf4",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 2,
+
+    instituicao: "Cursa",
+ 
+    curso: "Databricks",
+    
+    cargaHoraria: "9 h",
+
+    conclusaoISO: "2024-10-24",
+
+    dataConclusao: "24 de outubro de 2024",
+
+    codigo: "7451a4b4f1",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2024.10.24 · Cursa · DataBricks.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/7451a4b4f1",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
