5ab3158b45581f62801e4d738f91588b704640a1
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-10 13:39:32 -0300

    Update sistemas-operacionais-dataset.js

--- portfolio/formacoes-complementares/sistemas-operacionais/sistemas-operacionais-dataset.js
+++ portfolio/formacoes-complementares/sistemas-operacionais/sistemas-operacionais-dataset.js
@@ -47,5 +47,55 @@ window.datasetCategoria = [
       url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=ff03a7806ef30659780b9ff061b98936&action=view",
       texto: "Verificar Autenticidade"
     }
+  },
+
+  {
+    id: 3,
+
+    instituicao: "Instituto Federal Rio Grande do Sul",
+ 
+    curso: "Windows Server 2016: Instalação e Administração",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-10-06",
+
+    dataConclusao: "06 de outubro de 2025",
+
+    codigo: "68e41cf2-4018-4b7a-b928-e2810aa80106",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.10.06 · Instituto Federal Rio Grande do Sul · Windows Server 2016 (Instalação e Administração).png",
+
+    verificacao: {
+      url: "https://moodle.ifrs.edu.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 4,
+
+    instituicao: "Instituto Federal Rio Grande do Sul",
+ 
+    curso: "Windows Server 2016: Active Directory",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-10-06",
+
+    dataConclusao: "06 de outubro de 2025",
+
+    codigo: "68e41d95-ad74-4f81-a1d6-cec00aa8150d",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.10.06 · Instituto Federal Rio Grande do Sul · Windows Server 2016 (Active Directory).png",
+
+    verificacao: {
+      url: "https://moodle.ifrs.edu.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
   }
 ];
