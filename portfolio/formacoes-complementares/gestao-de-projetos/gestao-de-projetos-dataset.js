ca883a125b0dd4f4066dc86ad4be6f804faecd25
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-11 11:49:26 -0300

    Create gestao-de-projetos-dataset.js

---
+++ portfolio/formacoes-complementares/gestao-de-projetos/gestao-de-projetos-dataset.js
@@ -0,0 +1,92 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Fundamentos da Gestão de Projetos",
+    
+    cargaHoraria: "5 h",
+
+    conclusaoISO: "2024-03-03",
+
+    dataConclusao: "03 de março de 2024",
+
+    codigo: "328920",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-projetos/certificados/2024.03.03 · FM2S · Fundamentos da Gestão de Projetos.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 2,
+
+    instituicao: "HP Foundation",
+ 
+    curso: "Gestão Ágil de Projetos",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2024-10-12",
+
+    dataConclusao: "12 de outubro de 2024",
+
+    codigo: "2a663371-4e6e-4725-8741-849d4897fa49",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-projetos/certificados/2024.10.12 · HP Foundation · Gestão Ágil de Projetos.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 3,
+
+    instituicao: "Santander Open Academy",
+ 
+    curso: "Gestão de Projetos e Fudamentos de Métodos Agile",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-10-14",
+
+    dataConclusao: "14 de outubro de 2024",
+
+    codigo: "OA-2024-1014000607811",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-projetos/certificados/2024.10.14 · Santander Open Academy - Gestão de Projetos e Fudamentos de Métodos Agile.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 4,
+
+    instituicao: "Frons Educação",
+ 
+    curso: "Introdução à Gestão de Projetos",
+    
+    cargaHoraria: "5 h",
+
+    conclusaoISO: "2025-11-11",
+
+    dataConclusao: "11 de novembro de 2025",
+
+    codigo: "733D78A3B9-151374A24-144C20C72",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-projetos/certificados/2025.11.11 · Frons Educação · Introdução à Gestão de Projetos.png",
+
+    verificacao: {
+      url: "https://frons.com.br/verificacao-certificados/733D78A3B9-151374A24-144C20C72/",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
