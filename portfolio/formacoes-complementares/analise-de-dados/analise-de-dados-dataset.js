c7e30973d6e60df6b99010daaa5603613e283352
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-11 16:54:59 -0300

    Create analise-de-dados-dataset.js

---
+++ portfolio/formacoes-complementares/analise-de-dados/analise-de-dados-dataset.js
@@ -0,0 +1,221 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "Empowerdata",
+ 
+    curso: "Acelerador de Carreira com Power BI",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-01-11",
+
+    dataConclusao: "11 de janeiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.01.11 · Empowerdata · Acelerador de Carreira com Power BI.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 2,
+
+    instituicao: "Empowerdata",
+ 
+    curso: "Semana do Python™ na Prática",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-01-25",
+
+    dataConclusao: "25 de janeiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.01.25 · Empowerdata · Semana Python™ na Prática.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 3,
+
+    instituicao: "Tetra Educação",
+ 
+    curso: "Dashboards no Power BI",
+    
+    cargaHoraria: "6 h",
+
+    conclusaoISO: "2024-01-25",
+
+    dataConclusao: "25 de janeiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.01.25 · Tetra Educação · Dashboards no Power BI.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 4,
+
+    instituicao: "Xperiun | Data Analytics",
+ 
+    curso: "Data Expert: O Fim dos Relatórios Amadores",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2024-01-25",
+
+    dataConclusao: "25 de janeiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.01.25 · Xperiun · Data Analytics · Data Expert · O Fim dos Relatórios Amadores.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 5,
+
+    instituicao: "Datta Academy | Tech & Business",
+ 
+    curso: "Workshop Power BI",
+    
+    cargaHoraria: "6 h",
+
+    conclusaoISO: "2024-01-31",
+
+    dataConclusao: "31 de janeiro de 2024",
+
+    codigo: "ID1l2CW3o",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.01.31 · Datta Academy · Tech & Business · Workshop Power BI.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 6,
+
+    instituicao: "Escola Karen Abecia",
+ 
+    curso: "Excel Efeito UAU!",
+    
+    cargaHoraria: "7 h",
+
+    conclusaoISO: "2024-02-02",
+
+    dataConclusao: "02 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.02.02 · Escola Karen Abecia · Excel Efeito UAU!.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 7,
+
+    instituicao: "Nexos Educação",
+ 
+    curso: "Imersão Power BI",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-02-02",
+
+    dataConclusao: "02 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.02.02 · Nexos Educação · Imersão Power BI.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 8,
+
+    instituicao: "Inteligência de Manutenção",
+ 
+    curso: "Gestão de Manutenção com Power BI",
+    
+    cargaHoraria: "7 h",
+
+    conclusaoISO: "2024-02-08",
+
+    dataConclusao: "08 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.02.08 · Inteligência de Manutenção · Gestão de Manutenção com Power BI.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 9,
+
+    instituicao: "Simplifica Treinamentos",
+ 
+    curso: "Simplifica Excel Express",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2024-02-08",
+
+    dataConclusao: "08 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.02.08 · Simplifica Treinamentos · Simplifica Excel Express.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 10,
+
+    instituicao: "Tetra Educação",
+ 
+    curso: "Maratona do Excel",
+    
+    cargaHoraria: "6 h",
+
+    conclusaoISO: "2024-02-08",
+
+    dataConclusao: "08 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.02.08 · Tetra Educação · Maratona do Excel.png",
+
+    verificacao: null
+  }
+];
