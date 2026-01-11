b3ae3d895e8fbb3cc0c6c4a3b599a34ff10ab65c
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-11 17:32:11 -0300

    Update analise-de-dados-dataset.js

--- portfolio/formacoes-complementares/analise-de-dados/analise-de-dados-dataset.js
+++ portfolio/formacoes-complementares/analise-de-dados/analise-de-dados-dataset.js
@@ -217,5 +217,231 @@ window.datasetCategoria = [
     thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.02.08 · Tetra Educação · Maratona do Excel.png",
 
     verificacao: null
+  },
+
+  {
+    id: 11,
+
+    instituicao: "Fundação Getúlio Vargas",
+ 
+    curso: "Introdução à Ciência de Dados",
+    
+    cargaHoraria: "60 h",
+
+    conclusaoISO: "2024-02-21",
+
+    dataConclusao: "21 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.02.21 · Fundação Getúlio Vargas · Introdução à Ciência de Dados.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 12,
+
+    instituicao: "Hashtag Treinamentos",
+ 
+    curso: "Intensivão de Power BI",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-02-22",
+
+    dataConclusao: "22 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.02.22 · Hashtag Treinamentos · Intensivão de Power BI.png",
+
+    verificacao: {
+      url: "https://dlp.hashtagtreinamentos.com/certificado/download?ncf=Rogério+Clynton+Ribeiro&necf=Intensivão+de+Power+BI&dtcf=22%2F02%2F2024&hcf=8",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 13,
+
+    instituicao: "Preditiva Analytics",
+ 
+    curso: "Power BI para Análise de Dados",
+    
+    cargaHoraria: "12 h",
+
+    conclusaoISO: "2024-02-27",
+
+    dataConclusao: "27 de fevereiro de 2024",
+
+    codigo: "um9nw6lyaw8gumlizwlybw-0",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.02.27 · Preditiva Analytics · Power BI para Análise de Dados.png",
+
+    verificacao: {
+      url: "https://sites.preditiva.ai/certificados/um9nw6lyaw8gumlizwlybw-0",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 14,
+
+    instituicao: "Xperiun | Data Analytics",
+ 
+    curso: "Workshop: O Poder do Storytelling e Design de Dashboards",
+    
+    cargaHoraria: "3 h",
+
+    conclusaoISO: "2024-02-28",
+
+    dataConclusao: "28 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.02.25 · Xperiun · Data Analytics · Workshop (O Poder do Storytelling e Design de Dashboards).png",
+
+    verificacao: null
+  },
+
+  {
+    id: 15,
+
+    instituicao: "Grupo Tan",
+ 
+    curso: "Jornada Power BI",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-03-03",
+
+    dataConclusao: "03 de março de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.03.03 · Grupo Tan · Jornada Power BI.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 16,
+
+    instituicao: "Hashtag Treinamentos",
+ 
+    curso: "Jornada Python™ da Hashtag",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-03-07",
+
+    dataConclusao: "07 de março de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.03.07 · Hashtag Treinamentos · Jornada Python™ da Hashtag.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 17,
+
+    instituicao: "Tetra Educação",
+ 
+    curso: "Dashboards no Power BI",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-03-07",
+
+    dataConclusao: "07 de março de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.03.07 · Tetra Educação · Dashboards no Power BI.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 18,
+
+    instituicao: "Empowerdata",
+ 
+    curso: "Acelerador de Carreira com Power BI",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-03-14",
+
+    dataConclusao: "14 de março de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.03.14 · Empowerdata · Acelerador de Carreira com Power BI.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 19,
+
+    instituicao: "Simplifica Treinamentos",
+ 
+    curso: "Simplifica Power BI Express",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2024-03-14",
+
+    dataConclusao: "14 de março de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.03.14 · Simplifica Treinamentos · Simplifica Power BI Express.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 20,
+
+    instituicao: "Elite Analyst Solutions",
+ 
+    curso: "Analista de Elite com DAX (Data Analysis Expressions)",
+    
+    cargaHoraria: "4 h",
+
+    conclusaoISO: "2024-03-15",
+
+    dataConclusao: "15 de março de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.03.15 · Elite Analyst Solutions · Analista de Elite com DAX (Data Analysis Expressions).png",
+
+    verificacao: null
   }
 ];
