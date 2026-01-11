4d5982c06c4a19e5a88dffa68f434facc232fafd
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-11 12:14:30 -0300

    Create metodologia-agil-dataset.js

---
+++ portfolio/formacoes-complementares/metodologia-agil/metodologia-agil-dataset.js
@@ -0,0 +1,173 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Método Kanban",
+    
+    cargaHoraria: "12 h",
+
+    conclusaoISO: "2024-10-02",
+
+    dataConclusao: "02 de outubro de 2024",
+
+    codigo: "Y2VydGlmaWNhdGVfNDI3MDIyXzEwMDE%3D",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/metodologia-agil/certificados/2024.10.02 · FM2S · Método Kanban.png",
+
+    verificacao: {
+      url: "https://ead.fm2s.com.br/verify/Y2VydGlmaWNhdGVfNDI3MDIyXzEwMDE%3D",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 2,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "OKR - Objectives and Key Results",
+    
+    cargaHoraria: "5 h",
+
+    conclusaoISO: "2024-10-04",
+
+    dataConclusao: "04 de outubro de 2024",
+
+    codigo: "c5e02459600e00f748d0eb826b90241200081502",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/metodologia-agil/certificados/2024.10.04 · FM2S · OKR - Objectives and Key Results.png",
+
+    verificacao: {
+      url: "https://ead.fm2s.com.br/verify/c5e02459600e00f748d0eb826b90241200081502",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 3,
+
+    instituicao: "Qulture Rocks",
+ 
+    curso: "OKRs: Da Missão à Ação",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2025-03-24",
+
+    dataConclusao: "24 de março de 2025",
+
+    codigo: "cert_pcnfcb09",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/metodologia-agil/certificados/2025.03.24 · Qulture Rocks · OKRs · Da Missão à Ação.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 4,
+
+    instituicao: "Instituto Federal Rio Grande do Sul",
+ 
+    curso: "Scrum: Gerenciando Projetos de Software com Agilidade",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-09-21",
+
+    dataConclusao: "21 de setembro de 2025",
+
+    codigo: "68d02fb4-9efc-409b-b42e-dffc0aa8006f",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/metodologia-agil/certificados/2025.09.21 · Instituto Federal Rio Grande do Sul · Scrum (Gerenciando Projetos de Software com Agilidade).png",
+
+    verificacao: {
+      url: "https://moodle.ifrs.edu.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 5,
+
+    instituicao: "VMEdu, Inc",
+ 
+    curso: "OKR Fundamentals with Artificial Intelligence Certified",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2025-11-12",
+
+    dataConclusao: "12 de novembro de 2025",
+
+    codigo: "1125355",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/metodologia-agil/certificados/2025.11.12 · VMEdu, Inc · OKR Fundamentals with Artificial Intelligence Certified.png",
+
+    verificacao: {
+      url: "https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/OKRFundamentalswithAICertified-RogérioClyntonRibeiro-1125355.pdf",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 6,
+
+    instituicao: "VMEdu, Inc",
+ 
+    curso: "Kanban Essentials with Artificial Intelligence Certified",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2025-11-12",
+
+    dataConclusao: "12 de novembro de 2025",
+
+    codigo: "1125359",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/metodologia-agil/certificados/2025.11.12 · VMEdu, Inc · Kanban Essentials with Artificial Intelligence Certified.png",
+
+    verificacao: {
+      url: "https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/KanbanEssentialswithAICertified-RogérioClyntonRibeiro-1125359.pdf",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 7,
+
+    instituicao: "VMEdu, Inc",
+ 
+    curso: "Scrum Fundamentals Certified",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2025-11-14",
+
+    dataConclusao: "14 de novembro de 2025",
+
+    codigo: "1126415",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/metodologia-agil/certificados/2025.11.14 · VMEdu, Inc · Scrum Fundamentals Certified.png",
+
+    verificacao: {
+      url: "https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/ScrumFundamentalsCertified-RogérioClyntonRibeiro-1126415.pdf",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
