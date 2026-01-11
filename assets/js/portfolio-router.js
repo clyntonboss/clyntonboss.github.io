b0d153184727fb2220842fcbc3cba1607c2e7189
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-11 11:20:06 -0300

    Update portfolio-router.js

--- assets/js/portfolio-router.js
+++ assets/js/portfolio-router.js
@@ -250,6 +250,8 @@ document.addEventListener("click", e => {
   clearSideMenuActive();
   
   loadCategoryDataset(category.dataset, () => {
+    // 🔹 inicializa navegação do bloco
+    inicializarNavegacaoBloco();
   
     // 🔹 força modo inicial
     setViewMode("block");
