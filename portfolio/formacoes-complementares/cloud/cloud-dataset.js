c057f7d8d85fed2d8d5c543e2cae577887ff1e31
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-12 19:45:56 -0300

    Create cloud-dataset.js

---
+++ portfolio/formacoes-complementares/cloud/cloud-dataset.js
@@ -0,0 +1,23 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "D&K Cloud Architecture",
+ 
+    curso: "Docker & Kubernetes",
+    
+    cargaHoraria: "6 h",
+
+    conclusaoISO: "2024-02-21",
+
+    dataConclusao: "21 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2024.02.21 · D&K Cloud Architecture · Docker & Kubernetes.png",
+
+    verificacao: null
+  }
+];
