db031e291889bbc53b6aac38202997bf19b68026
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-12 16:39:40 -0300

    Create inteligencia-artificial-dataset.js

---
+++ portfolio/formacoes-complementares/inteligencia-artificial/inteligencia-artificial-dataset.js
@@ -0,0 +1,23 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "Faculdade Exame",
+ 
+    curso: "Inteligência Artificial: Carreira do Futuro",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2024-01-17",
+
+    dataConclusao: "17 de janeiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/inteligencia-artificial/certificados/2024.01.17 · Faculdade Exame · Inteligência Artificial (Carreira do Futuro).png",
+
+    verificacao: null
+  }
+];
