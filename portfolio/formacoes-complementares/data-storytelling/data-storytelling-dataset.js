870a0f829d1744aa1b2e33df64116028bafaad6c
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-12 02:59:27 -0300

    Update data-storytelling-dataset.js

--- portfolio/formacoes-complementares/data-storytelling/data-storytelling-dataset.js
+++ portfolio/formacoes-complementares/data-storytelling/data-storytelling-dataset.js
@@ -2,6 +2,31 @@ window.datasetCategoria = [
   {
     id: 1,
 
+    instituicao: "Escola Nacional de Administração Pública - ENAP",
+ 
+    curso: "Storytelling com Dados para Comunicação Profissional de Sucesso",
+    
+    cargaHoraria: "25 h",
+
+    conclusaoISO: "2024-10-11",
+
+    dataConclusao: "11 de outubro de 2024",
+
+    codigo: "4kXc14628908XEE8",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2024.10.11 · Escola Nacional de Administração Pública · Storytelling com Dados para Comunicação Profissional de Sucesso.png",
+
+    verificacao: {
+      url: "https://www.escolavirtual.gov.br/documentos/validacao/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 2,
+
     instituicao: "Simplifica Treinamentos",
  
     curso: "MasterClass PowerPoint",
