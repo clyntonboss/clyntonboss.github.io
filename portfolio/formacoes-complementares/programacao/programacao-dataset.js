f2f9633a3f8292d47253ce7efdef03c09d2f197f
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-12 23:59:22 -0300

    Create programacao-dataset.js

---
+++ portfolio/formacoes-complementares/programacao/programacao-dataset.js
@@ -0,0 +1,26 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "Hashtag Treinamentos",
+ 
+    curso: "Intensivão de JavaScript",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-02-01",
+
+    dataConclusao: "01 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/programacao/certificados/2024.02.01 · Hashtag Treinamentos · Intensivão de JavaScript.png",
+
+    verificacao: {
+      url: "https://dlp.hashtagtreinamentos.com/certificado/download?ncf=Rogério+Clynton+Ribeiro&necf=Intensivão+de+JavaScript&dtcf=01%2F02%2F2024&hcf=8",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
