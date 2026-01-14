562062d80ea2bf055fff4e4eda935b8f132c0f5f
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-14 13:22:53 -0300

    Update cloud-dataset.js

--- portfolio/formacoes-complementares/cloud/cloud-dataset.js
+++ portfolio/formacoes-complementares/cloud/cloud-dataset.js
@@ -546,5 +546,501 @@ window.datasetCategoria = [
       url: "https://credentials.databricks.com/3906926c-3c2a-4a78-9d83-7e84ed580cea#acc.6qvTaop8",
       texto: "Verificar Autenticidade"
     }
+  },
+
+  {
+    id: 25,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Fundamentals",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-06-11",
+
+    dataConclusao: "11 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.11 · Databricks Academy · Databricks Fundamentals.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 26,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Fundamentals Accreditation",
+    
+    cargaHoraria: "30 min",
+
+    conclusaoISO: "2025-06-11",
+
+    dataConclusao: "11 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.11 · Databricks Academy · Databricks Fundamentals Accreditation.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 27,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Accredited Databricks Fundamentals",
+    
+    cargaHoraria: "1 h 30 min",
+
+    conclusaoISO: "2025-06-11",
+
+    dataConclusao: "11 de junho de 2025 (Issue: June 11, 2025 / Expiration: June 11,2026)",
+
+    codigo: "64c80be0-4b36-4a4f-8d3d-3b10e5482c53#acc.QxLS6wM5",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.11 · Databricks Academy · Databricks Accredited Databricks Fundamentals.png",
+
+    verificacao: {
+      url: "https://credentials.databricks.com/64c80be0-4b36-4a4f-8d3d-3b10e5482c53#acc.QxLS6wM5",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 28,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Generative Artificial Intelligence Fundamentals",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-19",
+
+    dataConclusao: "19 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.19 · Databricks Academy · Generative Artificial Intelligence Fundamentals.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 29,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Generative Artificial Intelligence Fundamentals Accreditation",
+    
+    cargaHoraria: "10 min",
+
+    conclusaoISO: "2025-06-19",
+
+    dataConclusao: "19 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.19 · Databricks Academy · Generative Artificial Intelligence Fundamentals Accreditation.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 30,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Generative Artificial Intelligence Solution Development",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-19",
+
+    dataConclusao: "19 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.19 · Databricks Academy · Generative Artificial Intelligence Solution Development.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 31,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Generative Artificial Intelligence Application Development",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-19",
+
+    dataConclusao: "19 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.19 · Databricks Academy · Generative Artificial Intelligence Application Development.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 32,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Generative Artificial Intelligence Application Evaluation and Governance",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-19",
+
+    dataConclusao: "19 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.19 · Databricks Academy · Generative Artificial Intelligence Application Evaluation and Governance.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 33,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Generative Artificial Intelligence Application Deployment and Monitoring",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-19",
+
+    dataConclusao: "19 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.19 · Databricks Academy · Generative Artificial Intelligence Application Deployment and Monitoring.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 34,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Accredited Generative Artificial Intelligence Fundamentals",
+    
+    cargaHoraria: "10 h 10 min",
+
+    conclusaoISO: "2025-06-19",
+
+    dataConclusao: "19 de junho de 2025 (Issue: June 19, 2025 / Expiration: June 19, 2027)",
+
+    codigo: "8860b920-cc9e-44ef-b74f-d8dade5f4097#acc.wtGEnHhX",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.19 · Databricks Academy · Databricks Accredited Generative Artificial Intelligence Fundamentals.png",
+
+    verificacao: {
+      url: "https://credentials.databricks.com/8860b920-cc9e-44ef-b74f-d8dade5f4097#acc.wtGEnHhX",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 35,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Google Cloud Platform Databricks Platform Administration Fundamentals",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-06-20",
+
+    dataConclusao: "20 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.20 · Databricks Academy · Google Cloud Platform Databricks Platform Administration Fundamentals.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 36,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Google Cloud Platform Databricks Networking and Security Fundamentals",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-06-20",
+
+    dataConclusao: "20 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.20 · Databricks Academy · Google Cloud Platform Databricks Networking and Security Fundamentals.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 37,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Google Cloud Platform Databricks Cloud Integrations",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-06-20",
+
+    dataConclusao: "20 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.20 · Databricks Academy · Google Cloud Platform Databricks Cloud Integrations.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 38,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Google Cloud Platform Databricks Platform Architect Accreditation",
+    
+    cargaHoraria: "20 min",
+
+    conclusaoISO: "2025-06-20",
+
+    dataConclusao: "20 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.20 · Databricks Academy · Google Cloud Platform Databricks Platform Architect Accreditation.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 39,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Accredited Google Cloud Platform Databricks Platform Architect",
+    
+    cargaHoraria: "3 h 20 min",
+
+    conclusaoISO: "2025-06-21",
+
+    dataConclusao: "21 de junho de 2025 (Issue: June 21, 2025 / Expiration: June 21, 2027)",
+
+    codigo: "40006ffa-eb9f-4310-967c-2638c0f90ac1#acc.Czf2X0tp",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.21 · Databricks Academy · Databricks Accredited Google Cloud Platform Databricks Platform Architect.png",
+
+    verificacao: {
+      url: "https://credentials.databricks.com/40006ffa-eb9f-4310-967c-2638c0f90ac1#acc.Czf2X0tp",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 40,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Amazon Web Services Databricks Platform Administration Fundamentals",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-06-29",
+
+    dataConclusao: "29 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.29 · Databricks Academy · Amazon Web Services Databricks Platform Administration Fundamentals.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 41,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Amazon Web Services Databricks Networking and Security Fundamentals",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-06-29",
+
+    dataConclusao: "29 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.29 · Databricks Academy · Amazon Web Services Databricks Networking and Security Fundamentals.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 42,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Amazon Web Services Databricks Cloud Integrations",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-06-29",
+
+    dataConclusao: "29 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.29 · Databricks Academy · Amazon Web Services Databricks Cloud Integrations.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 43,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Amazon Web Services Databricks Platform Architect Accreditation",
+    
+    cargaHoraria: "20 min",
+
+    conclusaoISO: "2025-06-29",
+
+    dataConclusao: "29 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.29 · Databricks Academy · Amazon Web Services Databricks Platform Architect Accreditation.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 44,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Accredited Amazon Web Services Platform Architect",
+    
+    cargaHoraria: "3 h 20 min",
+
+    conclusaoISO: "2025-06-29",
+
+    dataConclusao: "29 de junho de 2025 (Issue: June 29, 2025 / Expiration: June 29, 2027)",
+
+    codigo: "84a4d7a4-f3bd-4671-b524-9999d80703e3#acc.H4zfQY7o",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.29 · Databricks Academy · Databricks Accredited Amazon Web Services Platform Architect.png",
+
+    verificacao: {
+      url: "https://credentials.databricks.com/84a4d7a4-f3bd-4671-b524-9999d80703e3#acc.H4zfQY7o",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 45,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Azure Databricks Workspace Administration Fundamentals",
+    
+    cargaHoraria: "1 h 10 min",
+
+    conclusaoISO: "2025-06-30",
+
+    dataConclusao: "30 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.30 · Databricks Academy · Azure Databricks Workspace Administration Fundamentals.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 46,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Azure Databricks Networking and Security Fundamentals",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-30",
+
+    dataConclusao: "30 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.30 · Databricks Academy · Azure Databricks Networking and Security Fundamentals.png",
+
+    verificacao: null
   }
 ];
