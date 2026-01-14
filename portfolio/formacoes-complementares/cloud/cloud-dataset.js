a15cf2256ed15c4314f7ca78d173bb5f7240fcc5
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-14 12:55:39 -0300

    Update cloud-dataset.js

--- portfolio/formacoes-complementares/cloud/cloud-dataset.js
+++ portfolio/formacoes-complementares/cloud/cloud-dataset.js
@@ -19,5 +19,532 @@ window.datasetCategoria = [
     thumb: "/portfolio/formacoes-complementares/cloud/certificados/2024.02.21 · D&K Cloud Architecture · Docker & Kubernetes.png",
 
     verificacao: null
+  },
+
+  {
+    id: 2,
+
+    instituicao: "Cursa",
+ 
+    curso: "Microsoft Azure",
+    
+    cargaHoraria: "14 h",
+
+    conclusaoISO: "2024-10-26",
+
+    dataConclusao: "26 de outubro de 2024",
+
+    codigo: "441b6d0de7",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2024.10.26 · Cursa · Microsoft Azure.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/441b6d0de7",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 3,
+
+    instituicao: "Cursa",
+ 
+    curso: "AWS do Zero ao Expert",
+    
+    cargaHoraria: "11 h",
+
+    conclusaoISO: "2024-10-28",
+
+    dataConclusao: "28 de outubro de 2024",
+
+    codigo: "2b8b611dd2",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2024.10.28 · Cursa · AWS do Zero ao Expert.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/2b8b611dd2",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 4,
+
+    instituicao: "Cursa",
+ 
+    curso: "SharePoint para Iniciantes",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-10-29",
+
+    dataConclusao: "29 de outubro de 2024",
+
+    codigo: "b101026adc",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2024.10.29 · Cursa · SharePoint para Iniciantes.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/b101026adc",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 5,
+
+    instituicao: "Cursa",
+ 
+    curso: "Google Cloud Platform",
+    
+    cargaHoraria: "11 h",
+
+    conclusaoISO: "2024-11-08",
+
+    dataConclusao: "08 de novembro de 2024",
+
+    codigo: "c0e4796e55",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2024.11.08 · Cursa · Google Cloud Platform.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/c0e4796e55",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 6,
+
+    instituicao: "Digital Innovation One",
+ 
+    curso: "Introdução ao Microsoft Azure Advanced",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2024-12-09",
+
+    dataConclusao: "09 de dezembro de 2024",
+
+    codigo: "ZMANG25S",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2024.12.09 · Digital Innovation One · Introdução ao Microsoft Azure Advanced.png",
+
+    verificacao: {
+      url: "https://hermes.dio.me/certificates/ZMANG25S.pdf",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 7,
+
+    instituicao: "TFTEC Cloud Company",
+ 
+    curso: "Microsoft Azure",
+    
+    cargaHoraria: "46 h",
+
+    conclusaoISO: "2025-02-06",
+
+    dataConclusao: "06 de fevereiro de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.02.06 · TFTEC Cloud Company · Microsoft Azure.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 8,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Cloud Fundamentals, Administration and Solution Architect",
+    
+    cargaHoraria: "80 h",
+
+    conclusaoISO: "2025-03-04",
+
+    dataConclusao: "04 de março de 2025",
+
+    codigo: "1d943ad0b9100be399f0059df696ac84",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.03.04 · FIAP - Centro Universitário · Cloud Fundamentals, Administration and Solution Architect.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=1d943ad0b9100be399f0059df696ac84&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 9,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Automate Production Workflows",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-02",
+
+    dataConclusao: "02 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.02 · Databricks Academy · Automate Production Workflows.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 10,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Knowledge Check: Get Started with Databricks for Data Engineering",
+    
+    cargaHoraria: "10 min",
+
+    conclusaoISO: "2025-06-02",
+
+    dataConclusao: "02 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.02 · Databricks Academy · Knowledge Check (Get Started with Databricks for Data Engineering).png",
+
+    verificacao: null
+  },
+
+  {
+    id: 11,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Get Started with SQL Analytics and BI on Databricks",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-03",
+
+    dataConclusao: "02 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.03 · Databricks Academy · Get Started with SQL Analytics and BI on Databricks.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 12,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Get Started with Lakehouse Architecture on Databricks",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-04",
+
+    dataConclusao: "04 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.04 · Databricks Academy · Get Started with Lakehouse Architecture on Databricks.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 13,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Get Started with Databricks Platform Administration",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-05",
+
+    dataConclusao: "05 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.05 · Databricks Academy · Get Started with Databricks Platform Administration.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 14,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Get Started with Databricks for Machine Learning",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-06",
+
+    dataConclusao: "06 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.06 · Databricks Academy · Get Started with Databricks for Machine Learning.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 15,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Get Started with Databricks for Generative Artificial Intelligence",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-07",
+
+    dataConclusao: "07 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.07 · Databricks Academy · Get Started with Databricks for Generative Artificial Intelligence.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 16,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Get Started with Databricks for Data Warehousing",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-08",
+
+    dataConclusao: "08 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.08 · Databricks Academy · Get Started with Databricks for Data Warehousing.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 17,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Get Started with Databricks for Data Engineering",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-09",
+
+    dataConclusao: "09 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.09 · Databricks Academy · Get Started with Databricks for Data Engineering.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 18,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Platform Administration Fundamentals",
+    
+    cargaHoraria: "1 h 23 min",
+
+    conclusaoISO: "2025-06-11",
+
+    dataConclusao: "11 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.11 · Databricks Academy · Databricks Platform Administration Fundamentals.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 19,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Identity Administration",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-06-11",
+
+    dataConclusao: "11 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.11 · Databricks Academy · Databricks Identity Administration.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 20,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Workspace Administration and Security",
+    
+    cargaHoraria: "1 h 14 min",
+
+    conclusaoISO: "2025-06-11",
+
+    dataConclusao: "11 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.11 · Databricks Academy · Databricks Workspace Administration and Security.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 21,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Compute Resource Administration",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-06-11",
+
+    dataConclusao: "11 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.11 · Databricks Academy · Databricks Compute Resource Administration.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 22,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "CI/CD Administration in Databricks",
+    
+    cargaHoraria: "1 h 39 min",
+
+    conclusaoISO: "2025-06-11",
+
+    dataConclusao: "11 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.11 · Databricks Academy · CI·CD Administration in Databricks.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 23,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Accredited Platform Administrator Accreditation",
+    
+    cargaHoraria: "20 min",
+
+    conclusaoISO: "2025-06-11",
+
+    dataConclusao: "11 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.11 · Databricks Academy · Databricks Accredited Platform Administrator Accreditation.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 24,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Databricks Accredited Platform Administrator",
+    
+    cargaHoraria: "8 h 36 min",
+
+    conclusaoISO: "2025-06-11",
+
+    dataConclusao: "11 de junho de 2025 (Issue: June 11, 2025 / Expiration: June 11, 2026)",
+
+    codigo: "3906926c-3c2a-4a78-9d83-7e84ed580cea#acc.6qvTaop8",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/cloud/certificados/2025.06.11 · Databricks Academy · Databricks Accredited Platform Administrator Accreditation.png",
+
+    verificacao: {
+      url: "https://credentials.databricks.com/3906926c-3c2a-4a78-9d83-7e84ed580cea#acc.6qvTaop8",
+      texto: "Verificar Autenticidade"
+    }
   }
 ];
