abf0ee3d3f428557010805c36da948b927b2b0c5
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-14 18:52:19 -0300

    Update programacao-dataset.js

--- portfolio/formacoes-complementares/programacao/programacao-dataset.js
+++ portfolio/formacoes-complementares/programacao/programacao-dataset.js
@@ -22,5 +22,221 @@ window.datasetCategoria = [
       url: "https://dlp.hashtagtreinamentos.com/certificado/download?ncf=Rogério+Clynton+Ribeiro&necf=Intensivão+de+JavaScript&dtcf=01%2F02%2F2024&hcf=8",
       texto: "Verificar Autenticidade"
     }
+  },
+
+  {
+    id: 2,
+
+    instituicao: "Rocketseat",
+ 
+    curso: "NLW Expert: Trilha de HTML | CSS | JavaScript",
+    
+    cargaHoraria: "5 h",
+
+    conclusaoISO: "2024-02-08",
+
+    dataConclusao: "08 de fevereiro de 2024",
+
+    codigo: "bb23052c-17b2-44cd-897a-8eb3d6484ece",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/programacao/certificados/2024.02.08 · Rocketseat · NLW Expert (Trilha de HTML · CSS · JavaScript).png",
+
+    verificacao: {
+      url: "https://app.rocketseat.com.br/certificates/bb23052c-17b2-44cd-897a-8eb3d6484ece",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 3,
+
+    instituicao: "Black Eagle Treinamentos [JOV!ANO]",
+ 
+    curso: "Lógica de Programação com Python™",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-05-29",
+
+    dataConclusao: "29 de maio de 2024",
+
+    codigo: "Y2VydGlmaWNhdGVfNil5MI8xNzg%3D",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/programacao/certificados/2024.05.29 · BLACK EAGLE TREINAMENTOS [JOV!ANO] · Lógica de Programação com Python™.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 4,
+
+    instituicao: "Fundação Bradesco",
+ 
+    curso: "Linguagem de Programação Python™",
+    
+    cargaHoraria: "18 h",
+
+    conclusaoISO: "2024-10-10",
+
+    dataConclusao: "10 de outubro de 2024",
+
+    codigo: "D5AE56A5-6CCC-4883-A584-E8C1CF6EAFA4",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/programacao/certificados/2024.10.10 · Fundação Bradesco · Linguagem de Programação Python™.png",
+
+    verificacao: {
+      url: "https://www.ev.org.br/validar-certificado/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 5,
+
+    instituicao: "Aplitech Foundation EaD",
+ 
+    curso: "HTML5 & CSS3",
+    
+    cargaHoraria: "5 h",
+
+    conclusaoISO: "2024-10-17",
+
+    dataConclusao: "17 de outubro de 2024",
+
+    codigo: "X6sraYRUft",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/programacao/certificados/2024.10.17 · Aplitech Foundation EAD · HTML5 · CSS3.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 6,
+
+    instituicao: "Cursa",
+ 
+    curso: "Power Apps",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-10-22",
+
+    dataConclusao: "22 de outubro de 2024",
+
+    codigo: "c419191d4b",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/programacao/certificados/2024.10.22 · Cursa · Power Apps.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/c419191d4b",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 7,
+
+    instituicao: "Fundação Bradesco",
+ 
+    curso: "Introdução à Programação Orientada a Objetos",
+    
+    cargaHoraria: "5 h",
+
+    conclusaoISO: "2024-11-08",
+
+    dataConclusao: "08 de novembro de 2024",
+
+    codigo: "D5F4279F-17D7-4EAD-B53D-8AA1C93314D6",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/programacao/certificados/2024.11.08 · Fundação Bradesco · Introdução à Programação Orientada a Objetos.png",
+
+    verificacao: {
+      url: "https://www.ev.org.br/validar-certificado/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 8,
+
+    instituicao: "Escola Nacional de Administração Pública - ENAP",
+ 
+    curso: "Aprendendo com Python™",
+    
+    cargaHoraria: "20 h",
+
+    conclusaoISO: "2024-11-09",
+
+    dataConclusao: "09 de novembro de 2024",
+
+    codigo: "qPoL146307585W5a",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/programacao/certificados/2024.11.09 · Escola Nacional de Administração Pública · Aprendendo com Python™.png",
+
+    verificacao: {
+      url: "https://www.escolavirtual.gov.br/documentos/validacao/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 9,
+
+    instituicao: "Fundação Bradesco",
+ 
+    curso: "Desenvolvimento Orientado a Objetos Utilizando a Linguagem Python™",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2024-11-09",
+
+    dataConclusao: "09 de novembro de 2024",
+
+    codigo: "00CFAAC0-8018-49A1-B526-84C4279CB486",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/programacao/certificados/2024.11.09 · Fundação Bradesco · Desenvolvimento Orientado a Objetos Utilizando a Linguagem Python™.png",
+
+    verificacao: {
+      url: "https://www.ev.org.br/validar-certificado/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 10,
+
+    instituicao: "Santander Open Academy",
+ 
+    curso: "Python™",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-11-09",
+
+    dataConclusao: "09 de novembro de 2024",
+
+    codigo: "OA-2024-1109000649689",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/programacao/certificados/2024.11.09 · Santander Open Academy · Python™.png",
+
+    verificacao: null
   }
 ];
