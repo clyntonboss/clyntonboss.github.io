575a4f64a0133f6eea3b7ad6f39c77a5633b8d46
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 15:01:13 -0300

    Update gestao-de-pessoas-dataset.js

--- portfolio/formacoes-complementares/gestao-de-pessoas/gestao-de-pessoas-dataset.js
+++ portfolio/formacoes-complementares/gestao-de-pessoas/gestao-de-pessoas-dataset.js
@@ -94,5 +94,96 @@ window.datasetCategoria = [
     thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.08.20 · Qulture Rocks · Da Gestão de Desempenho à Universidade Corporativa.png",
 
     verificacao: null
+  },
+
+  {
+    id: 5,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Jovens Líderes",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-08-28",
+
+    dataConclusao: "28 de agosto de 2025",
+
+    codigo: "982083a40722b474b11399c33a5cc012",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.08.28 · FIAP - Centro Universitário · Jovens Líderes.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=982083a40722b474b11399c33a5cc012&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 6,
+
+    instituicao: "HP Foundation",
+ 
+    curso: "Experiência do Cliente para Sucesso dos Negócios",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-10-06",
+
+    dataConclusao: "06 de outubro de 2025",
+
+    codigo: "669ae90d-b215-4cec-9c3e-a4c3b362f908",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.10.06 · HP Foundation · Experiência do Cliente para Sucesso dos Negócios.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 7,
+
+    instituicao: "HP Foundation",
+ 
+    curso: "Gestão de Relacionamento com Clientes",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2025-10-06",
+
+    dataConclusao: "06 de outubro de 2025",
+
+    codigo: "6b794d0d-9d74-4a2a-ae35-d091160c0ccf",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.10.06 · HP Foundation · Gestão de Relacionamento com Clientes.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 8,
+
+    instituicao: "Qulture Rocks",
+ 
+    curso: "Gestão de Desempenho para RH",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2025-10-21",
+
+    dataConclusao: "21 de outubro de 2025",
+
+    codigo: "35783_xQCURbnC13iAzayzCZVW_6128412",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.10.21 · Qulture Rocks · Gestão de Desempenho para RH.png",
+
+    verificacao: null
   }
 ];
