b4e85b942a6513782e8d39cae82f3502476f0eeb
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 18:18:48 -0300

    Create gestao-financeira.js

---
+++ portfolio/formacoes-complementares/gestao-financeira/gestao-financeira.js
@@ -0,0 +1,26 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Gestão Financeira de Empresas",
+    
+    cargaHoraria: "20 h",
+
+    conclusaoISO: "2025-04-21",
+
+    dataConclusao: "21 de abril de 2025",
+
+    codigo: "6c8a5531a603a83b7cdc386476bf1c14",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-financeira/certificados/2025.04.21 · FIAP - Centro Universitário · Gestão Financeira de Empresas.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=6c8a5531a603a83b7cdc386476bf1c14&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
