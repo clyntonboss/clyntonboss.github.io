2e1e60932ba4eb2a1ad9334d2bd05b5607b5071d
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 18:25:33 -0300

    Create gestao-logistica-dataset.js

---
+++ portfolio/formacoes-complementares/gestao-logistica/gestao-logistica-dataset.js
@@ -0,0 +1,26 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Fundamentos da Gestão Logística",
+    
+    cargaHoraria: "6 h",
+
+    conclusaoISO: "2024-10-01",
+
+    dataConclusao: "01 de outubro de 2024",
+
+    codigo: "Y2VydGlmaWNhdGVfNDI3MDIyXzczMg%3D%3D",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-logistica/certificados/2024.10.01 · FM2S · Fundamentos da Gestão Logística.png",
+
+    verificacao: {
+      url: "https://ead.fm2s.com.br/verify/Y2VydGlmaWNhdGVfNDI3MDIyXzczMg%3D%3D",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
