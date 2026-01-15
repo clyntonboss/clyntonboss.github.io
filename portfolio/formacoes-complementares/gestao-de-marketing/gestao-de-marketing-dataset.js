1cc21e0d9b5b00fa28d707bc14f44667f7c0d717
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 18:00:45 -0300

    Create gestao-de-marketing-dataset.js

---
+++ portfolio/formacoes-complementares/gestao-de-marketing/gestao-de-marketing-dataset.js
@@ -0,0 +1,270 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "Academia Rubens Sant'Anna",
+ 
+    curso: "Trade Marketing",
+    
+    cargaHoraria: "4 h 30 min",
+
+    conclusaoISO: "2024-02-08",
+
+    dataConclusao: "08 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2024.02.08 · Academia Rubens Sant Anna · Trade Marketing.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 2,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "LinkedIn Avançado: Construção de Marca Pessoal e Networking Profissional",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2024-10-16",
+
+    dataConclusao: "16 de outubro de 2024",
+
+    codigo: "Y2VydGlmaWNhdGVfNDI3MDIyXzExNDg%3D",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2024.10.16 · FM2S · LinkedIn Avançado (Construção de Marca Pessoal e Networking Profissional).png",
+
+    verificacao: {
+      url: "https://ead.fm2s.com.br/verify/Y2VydGlmaWNhdGVfNDI3MDIyXzExNDg%3D",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 3,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Marketing em Plataformas de Social Media",
+    
+    cargaHoraria: "60 h",
+
+    conclusaoISO: "2025-04-19",
+
+    dataConclusao: "19 de abril de 2025",
+
+    codigo: "3d1882e593e5852569d30327fe60634f",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2025.04.19 · FIAP - Centro Universitário · Marketing em Plataformas de Social Media.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=3d1882e593e5852569d30327fe60634f&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 4,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Mobile Marketing",
+    
+    cargaHoraria: "60 h",
+
+    conclusaoISO: "2025-04-20",
+
+    dataConclusao: "20 de abril de 2025",
+
+    codigo: "7bea81f593d70ee0adc9eacb0b54aca3",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2025.04.20 · FIAP - Centro Universitário · Mobile Marketing.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=7bea81f593d70ee0adc9eacb0b54aca3&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 5,
+
+    instituicao: "Universidade Cruzeiro do Sul",
+ 
+    curso: "Gestão Avançada de Marketing",
+    
+    cargaHoraria: "30 h",
+
+    conclusaoISO: "2025-10-24",
+
+    dataConclusao: "24 de outubro de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2025.10.24 · Universidade Cruzeiro do Sul · Gestão Avançada de Marketing.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 6,
+
+    instituicao: "VMEdu, Inc",
+ 
+    curso: "Marketing Strategy Fundamentals",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2025-11-13",
+
+    dataConclusao: "13 de novembro de 2025",
+
+    codigo: "1126070",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2025.11.13 · VMEdu, Inc · Marketing Strategy Fundamentals.png",
+
+    verificacao: {
+      url: "https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/MarketingStrategyFundamentals-RogérioClyntonRibeiro-1126070.pdf",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 7,
+
+    instituicao: "VMEdu, Inc",
+ 
+    curso: "Digital Marketing Fundamentals",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2025-11-13",
+
+    dataConclusao: "13 de novembro de 2025",
+
+    codigo: "1126092",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2025.11.13 · VMEdu, Inc · Digital Marketing Fundamentals.png",
+
+    verificacao: {
+      url: "https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/DigitalMarketingFundamentals-RogérioClyntonRibeiro-1126092.pdf",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 8,
+
+    instituicao: "VMEdu, Inc",
+ 
+    curso: "Marketing Research Fundamentals",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2025-11-14",
+
+    dataConclusao: "14 de novembro de 2025",
+
+    codigo: "1126408",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2025.11.14 · VMEdu, Inc · Marketing Research Fundamentals.png",
+
+    verificacao: {
+      url: "https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/MarketingResearchFundamentals-RogérioClyntonRibeiro-1126408.pdf",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 9,
+
+    instituicao: "Pontifícia Universidade Católica do Rio Grande do Sul",
+ 
+    curso: "Marketing e Promoção Pessoal",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2025-12-24",
+
+    dataConclusao: "24 de dezembro de 2025",
+
+    codigo: "236963-2343-1",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2025.12.24 · Pontifícia Universidade Católica do Rio Grande do Sul · Marketing e Promoção Pessoal.png",
+
+    verificacao: {
+      url: "https://webappcl.pucrs.br/inscricoes/verificacao/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 10,
+
+    instituicao: "Pontifícia Universidade Católica do Rio Grande do Sul",
+ 
+    curso: "Métricas e Mensuração de Resultados no Ambiente Digital",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2025-12-25",
+
+    dataConclusao: "25 de dezembro de 2025",
+
+    codigo: "236964-673-1",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2025.12.25 · Pontifícia Universidade Católica do Rio Grande do Sul · Métricas e Mensuração de Resultados no Ambiente Digital.png",
+
+    verificacao: {
+      url: "https://webappcl.pucrs.br/inscricoes/verificacao/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 11,
+
+    instituicao: "Pontifícia Universidade Católica do Rio Grande do Sul",
+ 
+    curso: "Dados, Pesquisa e Tendências: Como fazer Marketing Inteligente",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2025-12-26",
+
+    dataConclusao: "26 de dezembro de 2025",
+
+    codigo: "256361-317-1",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-marketing/certificados/2025.12.25 · Pontifícia Universidade Católica do Rio Grande do Sul · Dados, Pesquisa e Tendências (Como fazer Marketing Inteligente).png",
+
+    verificacao: {
+      url: "https://webappcl.pucrs.br/inscricoes/verificacao/",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
