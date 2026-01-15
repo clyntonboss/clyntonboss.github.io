0b0bdad2dd8348a98ca1d47129ced76711c9d08a
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 19:24:13 -0300

    Create design-dataset.js

---
+++ portfolio/formacoes-complementares/design/design-dataset.js
@@ -0,0 +1,26 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "Cursa",
+ 
+    curso: "Figma 2023",
+    
+    cargaHoraria: "17 h",
+
+    conclusaoISO: "2024-11-10",
+
+    dataConclusao: "10 de novembro de 2024",
+
+    codigo: "f5b74b4c3e",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/design/certificados/2024.11.10 · Cursa · Figma 2023.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/f5b74b4c3e",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
