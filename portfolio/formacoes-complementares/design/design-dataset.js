f5a94047190b0242d3a157e61f0a285eba78ee84
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 19:48:33 -0300

    Update design-dataset.js

--- portfolio/formacoes-complementares/design/design-dataset.js
+++ portfolio/formacoes-complementares/design/design-dataset.js
@@ -22,5 +22,230 @@ window.datasetCategoria = [
       url: "https://cursa.com.br/certificate/f5b74b4c3e",
       texto: "Verificar Autenticidade"
     }
+  },
+
+  {
+    id: 2,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "User Experience",
+    
+    cargaHoraria: "60 h",
+
+    conclusaoISO: "2025-03-02",
+
+    dataConclusao: "02 de março de 2025",
+
+    codigo: "e190dc2b75950973d57b0695a9ddbe3a",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/design/certificados/2025.03.02 · FIAP - Centro Universitário · User Experience.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=e190dc2b75950973d57b0695a9ddbe3a&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 3,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Design Gráfico",
+    
+    cargaHoraria: "140 h",
+
+    conclusaoISO: "2025-03-08",
+
+    dataConclusao: "08 de março de 2025",
+
+    codigo: "a44edf40f13360a5717668d052c0553e",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/design/certificados/2025.03.08 · FIAP - Centro Universitário · Design Gráfico.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=a44edf40f13360a5717668d052c0553e&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 4,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Design Thinking",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-03-09",
+
+    dataConclusao: "09 de março de 2025",
+
+    codigo: "b73b942c64a89433bbe9919a575915c9",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/design/certificados/2025.03.09 · FIAP - Centro Universitário · Design Thinking.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=b73b942c64a89433bbe9919a575915c9&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 5,
+
+    instituicao: "Instituto Federal Sul-Rio-Grandense",
+ 
+    curso: "Experiência do Usuário - UX",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-03-21",
+
+    dataConclusao: "21 de março de 2025",
+
+    codigo: "67ddb4bb-04bc-4526-9da7-a04b0a00c840",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/design/certificados/2025.03.21 · Instituto Federal Sul-Rio-Grandense · Experiência do Usuário - UX.png",
+
+    verificacao: {
+      url: "https://mundi.ifsul.edu.br/ava/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 6,
+
+    instituicao: "Instituto Federal Sul-Rio-Grandense",
+ 
+    curso: "Projeto de Interfaces Gráficas",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-03-23",
+
+    dataConclusao: "23 de março de 2025",
+
+    codigo: "67e06069-96d4-4ade-ba37-0a800a00c840",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/design/certificados/2025.03.23 · Instituto Federal Sul-Rio-Grandense · Projeto de Interfaces Gráficas.png",
+
+    verificacao: {
+      url: "https://mundi.ifsul.edu.br/ava/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 7,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Design Thinking - Process",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-04-07",
+
+    dataConclusao: "07 de abril de 2025",
+
+    codigo: "e8389734d98df4195e972e89895402cd",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/design/certificados/2025.04.07 · FIAP - Centro Universitário · Design Thinking - Process.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=e8389734d98df4195e972e89895402cd&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 8,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Game Design",
+    
+    cargaHoraria: "160 h",
+
+    conclusaoISO: "2025-04-12",
+
+    dataConclusao: "12 de abril de 2025",
+
+    codigo: "2b125ab0a203f10e8e8d3a0d62712db0",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/design/certificados/2025.04.12 · FIAP - Centro Universitário · Game Design.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=2b125ab0a203f10e8e8d3a0d62712db0&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 9,
+
+    instituicao: "Fundação Escola de Governo",
+ 
+    curso: "Design Thinking",
+    
+    cargaHoraria: "20 h",
+
+    conclusaoISO: "2025-09-18",
+
+    dataConclusao: "18 de setembro de 2025",
+
+    codigo: "tdaDyP9lH9",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/design/certificados/2025.09.18 · Fundação Escola do Governo · Design Thinking.png",
+
+    verificacao: {
+      url: "https://enavirtual.sc.gov.br/mod/customcert/verify_certificate.php?code=tdaDyP9lH9&qrcode=1",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 10,
+
+    instituicao: "Cursa",
+ 
+    curso: "Figma UI3",
+    
+    cargaHoraria: "32 h",
+
+    conclusaoISO: "2025-10-12",
+
+    dataConclusao: "12 de outubro de 2025",
+
+    codigo: "d0246a802a",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/design/certificados/2025.10.12 · Cursa · Figma UI3.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/d0246a802a",
+      texto: "Verificar Autenticidade"
+    }
   }
 ];
