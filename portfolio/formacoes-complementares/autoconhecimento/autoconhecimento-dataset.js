9e6bc24d92675b09cdc820de0151c0ae57016513
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 20:20:56 -0300

    Create autoconhecimento-dataset.js

---
+++ portfolio/formacoes-complementares/autoconhecimento/autoconhecimento-dataset.js
@@ -0,0 +1,26 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Desenvolvimento Profissional: Autoconhecimento",
+    
+    cargaHoraria: "14 h",
+
+    conclusaoISO: "2025-05-28",
+
+    dataConclusao: "28 de maio de 2025",
+
+    codigo: "cc3f758279298d9377686a0c214e4acbfb5547ff",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/autoconhecimento/certificados/2025.05.28 · FM2S · Desenvolvimento Profissional (Autoconhecimento).png",
+
+    verificacao: {
+      url: "https://ead.fm2s.com.br/verify/cc3f758279298d9377686a0c214e4acbfb5547ff",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
