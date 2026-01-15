7ca6f113921a83cc204ffea0a911c7b818e0ae9c
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 14:55:24 -0300

    Create gestao-de-pessoas-dataset.js

---
+++ portfolio/formacoes-complementares/gestao-de-pessoas/gestao-de-pessoas-dataset.js
@@ -0,0 +1,98 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Fundamentos da Gestão e Liderança",
+    
+    cargaHoraria: "5 h",
+
+    conclusaoISO: "2024-10-14",
+
+    dataConclusao: "14 de outubro de 2024",
+
+    codigo: "Y2VydGlmaWNhdGVfNDI3MDIyXzg5NQ%3D%3D",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2024.10.14 · FM2S · Fundamentos da Gestão e Liderança.png",
+
+    verificacao: {
+      url: "https://ead.fm2s.com.br/verify/Y2VydGlmaWNhdGVfNDI3MDIyXzg5NQ%3D%3D",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 2,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Customer Experience Management",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-04-17",
+
+    dataConclusao: "17 de abril de 2025",
+
+    codigo: "c032b68e8ca24c3f0664eb795bbaddc3",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.04.17 · FIAP - Centro Universitário · Customer Experience Management.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=c032b68e8ca24c3f0664eb795bbaddc3&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 3,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Leadership Communication",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-04-18",
+
+    dataConclusao: "18 de abril de 2025",
+
+    codigo: "a007b5fe21db52f7a94cc3f344b5bb7a",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.04.18 · FIAP - Centro Universitário · Leadership Communication.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=a007b5fe21db52f7a94cc3f344b5bb7a&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 4,
+
+    instituicao: "Qulture Rocks",
+ 
+    curso: "Da Gestão de Desempenho à Universidade Corporativa",
+    
+    cargaHoraria: "6 h",
+
+    conclusaoISO: "2025-08-20",
+
+    dataConclusao: "20 de agosto de 2025",
+
+    codigo: "35783_S50br3BexedAUqnUgZkL_6128412",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.08.20 · Qulture Rocks · Da Gestão de Desempenho à Universidade Corporativa.png",
+
+    verificacao: null
+  }
+];
