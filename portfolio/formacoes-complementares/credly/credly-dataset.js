972026d953f5b9fa949725a86e2ad89e9d3f89ee
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 20:56:48 -0300

    Create credly-dataset.js

---
+++ portfolio/formacoes-complementares/credly/credly-dataset.js
@@ -0,0 +1,23 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "IBM SkillsBuild",
+ 
+    curso: "Crie uma Conta Credly",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-10-21",
+
+    dataConclusao: "21 de outubro de 2025",
+
+    codigo: "URL-0E39749E2965",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/credly/certificados/2025.10.21 · IBM SkillsBuild · Crie uma Conta Credly.png",
+
+    verificacao: null
+  }
+];
