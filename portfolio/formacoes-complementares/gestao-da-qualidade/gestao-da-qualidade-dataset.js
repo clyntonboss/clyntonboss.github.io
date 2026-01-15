b27d2ae0c9252ee18dc0eb2a6a8c9506fb3126a2
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 13:33:46 -0300

    Create gestao-da-qualidade-dataset.js

---
+++ portfolio/formacoes-complementares/gestao-da-qualidade/gestao-da-qualidade-dataset.js
@@ -0,0 +1,227 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Lean Six Sigma White Belt",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-02-28",
+
+    dataConclusao: "28 de fevereiro de 2024",
+
+    codigo: "327940",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-da-qualidade/certificados/2024.02.28 · FM2S · Certificação Lean Six Sigma White Belt.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 2,
+
+    instituicao: "Produzindo Engenharia",
+ 
+    curso: "White Belt Lean Six Sigma",
+    
+    cargaHoraria: "6 h",
+
+    conclusaoISO: "2024-02-29",
+
+    dataConclusao: "29 de fevereiro de 2024",
+
+    codigo: "WBFEV240077",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-da-qualidade/certificados/2024.02.29 · Produzindo Engenharia · White Belt Lean Six Sigma.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 3,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Lean Six Sigma Yellow Belt",
+    
+    cargaHoraria: "24 h",
+
+    conclusaoISO: "2024-03-01",
+
+    dataConclusao: "01 de março de 2024",
+
+    codigo: "328571",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-da-qualidade/certificados/2024.03.01 · FM2S · Certificação Lean Six Sigma Yellow Belt.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 4,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Fundamentos da Gestão da Qualidade",
+    
+    cargaHoraria: "9 h",
+
+    conclusaoISO: "2024-03-02",
+
+    dataConclusao: "02 de março de 2024",
+
+    codigo: "328731",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-da-qualidade/certificados/2024.03.02 · FM2S · Fundamentos da Gestão da Qualidade.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 5,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Introdução ao Lean",
+    
+    cargaHoraria: "9 h",
+
+    conclusaoISO: "2024-03-02",
+
+    dataConclusao: "02 de março de 2024",
+
+    codigo: "328728",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-da-qualidade/certificados/2024.03.02 · FM2S · Introdução ao Lean.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 6,
+
+    instituicao: "Escola Yokoten",
+ 
+    curso: "Lean Six Sigma Yellow Belt",
+    
+    cargaHoraria: "12 h",
+
+    conclusaoISO: "2024-03-07",
+
+    dataConclusao: "07 de março de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-da-qualidade/certificados/2024.03.07 · Escola Yokoten · Lean Six Sigma Yellow Belt.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 7,
+
+    instituicao: "Instituto Politécnico de Ensino a Distância - iPed",
+ 
+    curso: "Lean Six Sigma",
+    
+    cargaHoraria: "80 h",
+
+    conclusaoISO: "2024-03-29",
+
+    dataConclusao: "29 de março de 2024",
+
+    codigo: "1588185",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-da-qualidade/certificados/2024.03.29 · Instituto Politécnico de Ensino a Distância · Lean Six Sigma.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 8,
+
+    instituicao: "Domus Lean Seis Sigma",
+ 
+    curso: "Lean Seis Sigma Green Belt",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2024-09-08",
+
+    dataConclusao: "08 de setembro de 2024",
+
+    codigo: "DLSSGB00006",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-da-qualidade/certificados/2024.09.08 · Domus Lean Seis Sigma · Lean Seis Sigma Green Belt.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 9,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Fundamentos da Gestão da Produção Industrial",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-09-30",
+
+    dataConclusao: "30 de setembro de 2024",
+
+    codigo: "Y2VydGlmaWNhdGVfNDI3MDIyXzYyMA%3D%3D",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-da-qualidade/certificados/2024.09.30 · FM2S · Fundamentos da Gestão da Produção Industrial.png",
+
+    verificacao: {
+      url: "https://ead.fm2s.com.br/verify/Y2VydGlmaWNhdGVfNDI3MDIyXzYyMA%3D%3D",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 10,
+
+    instituicao: "FM2S Educação e Consultoria",
+ 
+    curso: "Kaizen - Processo de Melhoria Contínua",
+    
+    cargaHoraria: "12 h",
+
+    conclusaoISO: "2024-10-03",
+
+    dataConclusao: "03 de outubro de 2024",
+
+    codigo: "Y2VydGlmaWNhdGVfNDI3MDIyXzI3OQ%3D%3D",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/gestao-da-qualidade/certificados/2024.10.03 · FM2S · Kaizen - Processo de Melhoria Contínua.png",
+
+    verificacao: {
+      url: "https://ead.fm2s.com.br/verify/Y2VydGlmaWNhdGVfNDI3MDIyXzI3OQ%3D%3D",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
