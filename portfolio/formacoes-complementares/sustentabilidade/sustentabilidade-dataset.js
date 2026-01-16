f250c14ebb3bda3b505ef804d7d4166666610bd0
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-16 00:41:45 -0300

    Update sustentabilidade-dataset.js

--- portfolio/formacoes-complementares/sustentabilidade/sustentabilidade-dataset.js
+++ portfolio/formacoes-complementares/sustentabilidade/sustentabilidade-dataset.js
@@ -19,5 +19,218 @@ window.datasetCategoria = [
     thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2024.02.21 · Faculdade Exame · Jornada ESG (Carreira em Sustentabilidade).png",
 
     verificacao: null
+  },
+
+  {
+    id: 2,
+
+    instituicao: "HP Foundation",
+ 
+    curso: "Economia Circular",
+    
+    cargaHoraria: "1 h",
+
+    conclusaoISO: "2024-10-11",
+
+    dataConclusao: "11 de outubro de 2024",
+
+    codigo: "73c436f1-af1c-40c6-81a8-5cfda781a275",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2024.10.11 · HP Foundation · Economia Circular.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 3,
+
+    instituicao: "Circular Academy",
+ 
+    curso: "Introdução à Economia Circular",
+    
+    cargaHoraria: "6 h",
+
+    conclusaoISO: "2024-10-25",
+
+    dataConclusao: "25 de outubro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2024.10.25 · Circular Academy · Introdução à Economia Circular.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 4,
+
+    instituicao: "Universidade Federal de Paraná",
+ 
+    curso: "Economia Circular",
+    
+    cargaHoraria: "6 h",
+
+    conclusaoISO: "2024-10-26",
+
+    dataConclusao: "26 de outubro de 2024",
+
+    codigo: "671c822f-4fa8-40dc-a137-008b0afc6115",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2024.10.26 · Universidade Federal de Paraná · Economia Circular.png",
+
+    verificacao: {
+      url: "https://ufpraberta.ufpr.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 5,
+
+    instituicao: "Instituto Federal Sul-Rio-Grandense",
+ 
+    curso: "Economia Circular",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2024-10-31",
+
+    dataConclusao: "31 de outubro de 2024",
+
+    codigo: "67237e7d-aa08-4fed-9671-8d1c0a00c840",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2024.10.31 · Instituto Federal Sul-Rio-Grandense · Economia Circular.png",
+
+    verificacao: {
+      url: "https://mundi.ifsul.edu.br/ava/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 6,
+
+    instituicao: "Pontifícia Universidade Católica do Rio Grande do Sul",
+ 
+    curso: "Economia Circular: Novos Modelos de Negócios Sustentáveis",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2024-11-06",
+
+    dataConclusao: "06 de novembro de 2024",
+
+    codigo: "236950-243-1",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2024.11.06 · Pontifícia Universidade Católica do Rio Grande do Sul · Economia Circular (Novos Modelos de Negócios Sustentáveis).png",
+
+    verificacao: {
+      url: "https://webappcl.pucrs.br/inscricoes/verificacao/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 7,
+
+    instituicao: "Capacita Coop",
+ 
+    curso: "Economia Circular",
+    
+    cargaHoraria: "30 min",
+
+    conclusaoISO: "2025-03-24",
+
+    dataConclusao: "24 de março de 2025",
+
+    codigo: "dd4ac838-1d17-40ad-adfc-44546ab27a0e",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2025.03.24 · Capacita Coop · Economia Circular.png",
+
+    verificacao: {
+      url: "https://studion-sescoop.dotgroup.com.br/external/validate-certificate/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 8,
+
+    instituicao: "Universidade Federal de São Carlos",
+ 
+    curso: "Economia Circular",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2025-03-26",
+
+    dataConclusao: "26 de março de 2025",
+
+    codigo: "67e433c8-f628-4555-9837-659eac1e1951",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2025.03.26 · Universidade Federal de São Carlos · Economia Circular.png",
+
+    verificacao: {
+      url: "https://cursos.poca.ufscar.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+
+  {
+    id: 9,
+
+    instituicao: "Urban Shift City Academy",
+ 
+    curso: "Circular Economy Strategies for Sustainable Development",
+    
+    cargaHoraria: "16 h",
+
+    conclusaoISO: "2025-04-01",
+
+    dataConclusao: "01 de abril de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2025.04.01 · Urban Shift City Academy · Circular Economy Strategies for Sustainable Development.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 10,
+
+    instituicao: "SENAI São Paulo",
+ 
+    curso: "Economia Circular",
+    
+    cargaHoraria: "20 h",
+
+    conclusaoISO: "2025-04-04",
+
+    dataConclusao: "04 de abril de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2025.04.04 · SENAI São Paulo · Economia Circular.png",
+
+    verificacao: null
   }
 ];
