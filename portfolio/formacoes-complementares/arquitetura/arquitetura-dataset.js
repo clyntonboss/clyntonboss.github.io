909dbc967cf5f9b8abdb954b3f14021082c500a1
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-15 21:06:02 -0300

    Create arquitetura-dataset.js

---
+++ portfolio/formacoes-complementares/arquitetura/arquitetura-dataset.js
@@ -0,0 +1,89 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "Architecture Solutions",
+ 
+    curso: "Jornada Especialista BIM",
+    
+    cargaHoraria: "9 h",
+
+    conclusaoISO: "2024-02-01",
+
+    dataConclusao: "01 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/arquitetura/certificados/2024.02.01 · Architecture Solutions · Jornada Especialista BIM.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 2,
+
+    instituicao: "Growarq",
+ 
+    curso: "Semana Growarq: Modelagem e Representação de Projetos Arquitetônicos",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2024-02-01",
+
+    dataConclusao: "01 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/arquitetura/certificados/2024.02.01 · Growarq · Semana Growarq · Modelagem e Representação de Projetos Arquitetônicos.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 3,
+
+    instituicao: "Rafael Soares",
+ 
+    curso: "Jornada Civil 3D",
+    
+    cargaHoraria: "6 h",
+
+    conclusaoISO: "2024-02-04",
+
+    dataConclusao: "04 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/arquitetura/certificados/2024.02.04 · Rafael Soares · Jornada Civil 3D.png",
+
+    verificacao: null
+  },
+
+  {
+    id: 4,
+
+    instituicao: "Inova Civil",
+ 
+    curso: "Intensivão do CAD ao BIM",
+    
+    cargaHoraria: "9 h",
+
+    conclusaoISO: "2024-03-07",
+
+    dataConclusao: "07 de março de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/arquitetura/certificados/2024.03.07 · Inova Civil · Intensivão do CAD ao BIM.png",
+
+    verificacao: null
+  }
+];
