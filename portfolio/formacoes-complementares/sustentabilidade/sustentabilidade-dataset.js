a0aedca77101a0345e15145457a9e54894ba63c7
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-16 00:22:07 -0300

    Create sustentabilidade-dataset.js

---
+++ portfolio/formacoes-complementares/sustentabilidade/sustentabilidade-dataset.js
@@ -0,0 +1,23 @@
+window.datasetCategoria = [
+  {
+    id: 1,
+
+    instituicao: "Faculdade Exame",
+ 
+    curso: "Jornada ESG: Carreira em Sustentabilidade",
+    
+    cargaHoraria: "1 h 10 min",
+
+    conclusaoISO: "2024-02-21",
+
+    dataConclusao: "21 de fevereiro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/sustentabilidade/certificados/2024.02.21 · Faculdade Exame · Jornada ESG (Carreira em Sustentabilidade).png",
+
+    verificacao: null
+  }
+];
