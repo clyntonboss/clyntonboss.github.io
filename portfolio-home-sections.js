652883c1e59129b66ee1232949880f95a421836b
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-01-17 22:39:33 -0300

    Create portfolio-home-sections.js

---
+++ portfolio-home-sections.js
@@ -0,0 +1,12 @@
+const conteudos = {
+  home: `
+    <section id="inicio" class="main-section fade-item">
+      <!-- todo o HTML da Home aqui -->
+    </section>
+  `,
+  portfolio: `
+    <section id="curriculo" class="main-section fade-item">
+      <!-- todo o HTML do Portfólio aqui -->
+    </section>
+  `
+};
