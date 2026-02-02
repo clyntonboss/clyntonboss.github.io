6d5f33e4581e656e62c445a84306bbc2c1eaa1ec
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-02 15:08:20 -0300

    Update skills-dataset.js

--- portfolio/formacoes-complementares/skills/skills-dataset.js
+++ portfolio/formacoes-complementares/skills/skills-dataset.js
@@ -1,27 +1,5 @@
 window.datasetCategoria = [
   {
-    id: 1,
-
-    instituicao: "IBM SkillsBuild",
- 
-    curso: "Faça por Merecer! Aceite! Compartilhe! | IBM SkillsBuild",
-    
-    cargaHoraria: "20 min",
-
-    conclusaoISO: "2025-10-21",
-
-    dataConclusao: "21 de outubro de 2025",
-
-    codigo: "URL-CC7432BB7A8A",
-
-    mostrarCopiar: false,
-
-    thumb: "/portfolio/formacoes-complementares/skills/certificados/2025.10.21 · IBM SkillsBuild · Faça por Merecer! Aceite! Compartilhe! IBM SkillsBuild.png",
-
-    verificacao: null
-  },
-
-  {
     id: 2,
 
     instituicao: "VMEdu, Inc",
@@ -44,5 +22,27 @@ window.datasetCategoria = [
       url: "https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/NegotiationAssociate-RogérioClyntonRibeiro-1126711.pdf",
       texto: "Verificar Autenticidade"
     }
+  },
+
+  {
+    id: 1,
+
+    instituicao: "IBM SkillsBuild",
+ 
+    curso: "Faça por Merecer! Aceite! Compartilhe! | IBM SkillsBuild",
+    
+    cargaHoraria: "20 min",
+
+    conclusaoISO: "2025-10-21",
+
+    dataConclusao: "21 de outubro de 2025",
+
+    codigo: "URL-CC7432BB7A8A",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/skills/certificados/2025.10.21 · IBM SkillsBuild · Faça por Merecer! Aceite! Compartilhe! IBM SkillsBuild.png",
+
+    verificacao: null
   }
 ];
