acbf869f83bc564b83e3dbf23ac4745dee58a426
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-02 13:41:50 -0300

    Create sistemas-operacionais-dataset.js

---
+++ portfolio/formacoes-complementares/sistemas-operacionais/sistemas-operacionais-dataset.js
@@ -0,0 +1,126 @@
+window.datasetCategoria = [
+  {
+    id: 5,
+
+    instituicao: "Cursa",
+ 
+    curso: "Máquinas Virtuais e Instalação de Sistemas",
+    
+    cargaHoraria: "9 h",
+
+    conclusaoISO: "2025-10-15",
+
+    dataConclusao: "15 de outubro de 2025",
+
+    codigo: "727ec39241",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.10.15 · Cursa · Máquinas Virtuais e Instalação de Sistemas.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/727ec39241",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 4,
+
+    instituicao: "Instituto Federal Rio Grande do Sul",
+ 
+    curso: "Windows Server 2016: Active Directory",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-10-06",
+
+    dataConclusao: "06 de outubro de 2025",
+
+    codigo: "68e41d95-ad74-4f81-a1d6-cec00aa8150d",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.10.06 · Instituto Federal Rio Grande do Sul · Windows Server 2016 (Active Directory).png",
+
+    verificacao: {
+      url: "https://moodle.ifrs.edu.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 3,
+
+    instituicao: "Instituto Federal Rio Grande do Sul",
+ 
+    curso: "Windows Server 2016: Instalação e Administração",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-10-06",
+
+    dataConclusao: "06 de outubro de 2025",
+
+    codigo: "68e41cf2-4018-4b7a-b928-e2810aa80106",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.10.06 · Instituto Federal Rio Grande do Sul · Windows Server 2016 (Instalação e Administração).png",
+
+    verificacao: {
+      url: "https://moodle.ifrs.edu.br/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 2,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Android",
+    
+    cargaHoraria: "80 h",
+
+    conclusaoISO: "2025-04-15",
+
+    dataConclusao: "15 de abril de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.04.15 · FIAP Centro Universitário · Android.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=ff03a7806ef30659780b9ff061b98936&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 1,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Linux Fundamentos",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-03-10",
+
+    dataConclusao: "10 de março de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.03.10 · FIAP Centro Universitário · Linux Fundamentos.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=b680d2fffda89a1e7ad608953a3f7fac&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
