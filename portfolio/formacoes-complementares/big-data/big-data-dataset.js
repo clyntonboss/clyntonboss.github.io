8fd58406845218ba5ec17a992c7c3711e9889372
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-02 08:01:38 -0300

    Create big-data-dataset.js

---
+++ portfolio/formacoes-complementares/big-data/big-data-dataset.js
@@ -0,0 +1,286 @@
+window.datasetCategoria = [
+  {
+    id: 12,
+
+    instituicao: "Escola Nacional de Administração Pública - ENAP",
+ 
+    curso: "Big Data em Apoio à Tomada de Decisão",
+    
+    cargaHoraria: "25 h",
+
+    conclusaoISO: "2025-10-27",
+
+    dataConclusao: "27 de outubro de 2025",
+
+    codigo: "wGcn19303427zgEG",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2025.10.27 · Escola Nacional de Administração Pública · Big Data em Apoio à Tomada de Decisão.png",
+
+    verificacao: {
+      url: "https://www.escolavirtual.gov.br/documentos/validacao/",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 11,
+
+    instituicao: "Cursa",
+ 
+    curso: "Big Data",
+    
+    cargaHoraria: "9 h",
+
+    conclusaoISO: "2025-08-15",
+
+    dataConclusao: "15 de agosto de 2025",
+
+    codigo: "485a2b2678",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2025.08.13 · Cursa · Big Data.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/485a2b2678",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 10,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Monitoring and Optimizing Apache™ Spark Workloads on Databricks",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-22",
+
+    dataConclusao: "22 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2025.06.22 · Databricks Academy · Monitoring and Optimizing Apache™ Spark Workloads on Databricks.png",
+
+    verificacao: null
+  },
+  
+  {
+    id: 9,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Stream Processing and Analysis with Apache™ Spark",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-22",
+
+    dataConclusao: "22 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2025.06.22 · Databricks Academy · Stream Processing and Analysis with Apache™ Spark.png",
+
+    verificacao: null
+  },
+  
+  {
+    id: 8,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Developing Applications with Apache™ Spark",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-22",
+
+    dataConclusao: "22 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2025.06.22 · Databricks Academy · Developing Applications with Apache™ Spark.png",
+
+    verificacao: null
+  },
+  
+  {
+    id: 7,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Introduction to Apache™ Spark",
+    
+    cargaHoraria: "2 h",
+
+    conclusaoISO: "2025-06-22",
+
+    dataConclusao: "22 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2025.06.22 · Databricks Academy · Introduction to Apache™ Spark.png",
+
+    verificacao: null
+  },
+  
+  {
+    id: 6,
+
+    instituicao: "Databricks Academy",
+ 
+    curso: "Introduction to Python™ for Data Science and Data Engineering",
+    
+    cargaHoraria: "12 h",
+
+    conclusaoISO: "2025-06-22",
+
+    dataConclusao: "22 de junho de 2025",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2025.06.22 · Databricks Academy · Introduction to Python™ for Data Science and Data Engineering.png",
+
+    verificacao: null
+  },
+  
+  {
+    id: 5,
+
+    instituicao: "Instituto Federal Minas Gerais",
+ 
+    curso: "Big Data - Avançado e Mineração de Dados",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-05-07",
+
+    dataConclusao: "07 de maio de 2025",
+
+    codigo: "681b959d-8be8-42cd-95a4-ed13c883362e",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2025.05.07 · Instituto Federal Minas Gerais · Big Data · Avançado e Mineração de Dados.png",
+
+    verificacao: {
+      url: "https://mais.ifmg.edu.br/maisifmg/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 4,
+
+    instituicao: "Instituto Federal Minas Gerais",
+ 
+    curso: "Big Data - Básico",
+    
+    cargaHoraria: "40 h",
+
+    conclusaoISO: "2025-05-06",
+
+    dataConclusao: "06 de maio de 2025",
+
+    codigo: "681a5f66-0368-4620-8361-348ec883362e",
+
+    mostrarCopiar: true,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2025.05.06 · Instituto Federal Minas Gerais · Big Data · Básico.png",
+
+    verificacao: {
+      url: "https://mais.ifmg.edu.br/maisifmg/mod/simplecertificate/verify.php",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 3,
+
+    instituicao: "FIAP - Centro Universitário",
+ 
+    curso: "Big Data & Analytics",
+    
+    cargaHoraria: "60 h",
+
+    conclusaoISO: "2025-02-28",
+
+    dataConclusao: "28 de fevereiro de 2025",
+
+    codigo: "0c36171ed8cb4f23e4b7e7148068eff8",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2025.02.28 · FIAP - Centro Universitário · Big Data & Analytics.png",
+
+    verificacao: {
+      url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=0c36171ed8cb4f23e4b7e7148068eff8&action=view",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 2,
+
+    instituicao: "Cursa",
+ 
+    curso: "Databricks",
+    
+    cargaHoraria: "9 h",
+
+    conclusaoISO: "2024-10-24",
+
+    dataConclusao: "24 de outubro de 2024",
+
+    codigo: "7451a4b4f1",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2024.10.24 · Cursa · DataBricks.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/7451a4b4f1",
+      texto: "Verificar Autenticidade"
+    }
+  },
+  
+  {
+    id: 1,
+
+    instituicao: "Cursa",
+ 
+    curso: "Google BigQuery",
+    
+    cargaHoraria: "10 h",
+
+    conclusaoISO: "2024-10-23",
+
+    dataConclusao: "23 de outubro de 2024",
+
+    codigo: "ff39b8bbf4",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/big-data/certificados/2024.10.23 · Cursa · Google BigQuery.png",
+
+    verificacao: {
+      url: "https://cursa.com.br/certificate/ff39b8bbf4",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
