54e49f106b3616fa074195a7ab56da1f2593ea80
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-18 08:01:05 -0300

    Create matematica-dataset.js

---
+++ portfolio/formacoes-complementares/matematica/matematica-dataset.js
@@ -0,0 +1,26 @@
+window.datasetCategoria = [
+{
+    id: 1,
+
+    instituicao: "Didática Tech",
+ 
+    curso: "Matemática para Machine Learning",
+    
+    cargaHoraria: "8 h",
+
+    conclusaoISO: "2025-11-07",
+
+    dataConclusao: "07 de novembro de 2024",
+
+    codigo: "Inaplicável",
+
+    mostrarCopiar: false,
+
+    thumb: "/portfolio/formacoes-complementares/matematica/certificados/2025.11.07 · Didática Tech · Matemática para Machine Learning.png",
+
+    verificacao: {
+      url: "https://www.escolavirtual.gov.br/documentos/validacao/",
+      texto: "Verificar Autenticidade"
+    }
+  }
+];
