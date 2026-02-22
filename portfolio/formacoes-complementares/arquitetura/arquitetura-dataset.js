bf65ef0b777d522a561be57eef6f3fd495ed4f16
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-22 18:04:28 -0300

    Update arquitetura-dataset.js

--- portfolio/formacoes-complementares/arquitetura/arquitetura-dataset.js
+++ portfolio/formacoes-complementares/arquitetura/arquitetura-dataset.js
@@ -4,6 +4,13 @@ window.datasetCategoria = [
 
     thumb: "/portfolio/formacoes-complementares/arquitetura/certificados/2024.03.07 · Inova Civil · Intensivão do CAD ao BIM.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
     instituicao: "Inova Civil",
     curso: "Intensivão do CAD ao BIM",
     cargaHoraria: "9 h",
@@ -18,6 +25,13 @@ window.datasetCategoria = [
     id: 3,
 
     thumb: "/portfolio/formacoes-complementares/arquitetura/certificados/2024.02.04 · Rafael Soares · Jornada Civil 3D.png",
+
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
 
     instituicao: "Rafael Soares",
     curso: "Jornada Civil 3D",
@@ -34,6 +48,13 @@ window.datasetCategoria = [
 
     thumb: "/portfolio/formacoes-complementares/arquitetura/certificados/2024.02.01 · Growarq · Semana Growarq · Modelagem e Representação de Projetos Arquitetônicos.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
     instituicao: "Growarq",
     curso: "Semana Growarq: Modelagem e Representação de Projetos Arquitetônicos",
     cargaHoraria: "8 h",
@@ -48,6 +69,13 @@ window.datasetCategoria = [
     id: 1,
 
     thumb: "/portfolio/formacoes-complementares/arquitetura/certificados/2024.02.01 · Architecture Solutions · Jornada Especialista BIM.png",
+
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
 
     instituicao: "Architecture Solutions",
     curso: "Jornada Especialista BIM",
