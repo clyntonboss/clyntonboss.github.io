1968b03c4e2059c700ec0987f2119c8da2cef791
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-23 00:19:51 -0300

    Update gestao-de-operacoes-dataset.js

--- portfolio/formacoes-complementares/gestao-de-operacoes/gestao-de-operacoes-dataset.js
+++ portfolio/formacoes-complementares/gestao-de-operacoes/gestao-de-operacoes-dataset.js
@@ -2,69 +2,99 @@ window.datasetCategoria = [
   {
     id: 3,
 
-    instituicao: "Modular Cursos",
- 
-    curso: "Manutenção Preditiva: Técnicas e Ferramentas",
-    
-    cargaHoraria: "3 h",
-
-    conclusaoISO: "2025-05-06",
-
-    dataConclusao: "06 de maio de 2025",
-
-    codigo: "Inaplicável",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-operacoes/certificados/2025.05.06 · Modular Cursos · Manutenção Preditiva (Técnicas e Ferramentas).png",
 
-    verificacao: null
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Modular Cursos",
+    curso: "Manutenção Preditiva: Técnicas e Ferramentas",
+    cargaHoraria: "3 h",
+    conclusaoISO: "2025-05-06",
+    dataConclusao: "06 de maio de 2025",
+    codigo: "Inaplicável",
+    mostrarCopiar: false,
+    verificacao: null,
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 2,
 
-    instituicao: "Mecânica Total Brasil",
- 
-    curso: "Bombas Centrífugas",
-    
-    cargaHoraria: "5 h",
-
-    conclusaoISO: "2025-03-07",
-
-    dataConclusao: "07 de março de 2025",
-
-    codigo: "18309375.82560089.6.8.83093758256008968",
-
-    mostrarCopiar: true,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-operacoes/certificados/2024.03.07 · Mecânica Total Brasil · Bombas Centrígugas.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Mecânica Total Brasil",
+    curso: "Bombas Centrífugas",
+    cargaHoraria: "5 h",
+    conclusaoISO: "2025-03-07",
+    dataConclusao: "07 de março de 2025",
+    codigo: "18309375.82560089.6.8.83093758256008968",
+    mostrarCopiar: true,
     verificacao: {
       url: "https://www.even3.com.br/documentos/",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 1,
 
-    instituicao: "Modular Cursos",
- 
-    curso: "Indicadores de Manutenção",
-    
-    cargaHoraria: "3 h",
-
-    conclusaoISO: "2024-02-28",
-
-    dataConclusao: "28 de fevereiro de 2024",
-
-    codigo: "Inaplicável",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-operacoes/certificados/2024.02.28 · Modular Cursos · Indicadores de Manutenção.png",
 
-    verificacao: null
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Modular Cursos",
+    curso: "Indicadores de Manutenção",
+    cargaHoraria: "3 h",
+    conclusaoISO: "2024-02-28",
+    dataConclusao: "28 de fevereiro de 2024",
+    codigo: "Inaplicável",
+    mostrarCopiar: false,
+    verificacao: null,
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   }
 ];
