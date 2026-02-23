f276cae9c8616d585708133801da497d08e77dfc
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-23 00:24:09 -0300

    Update gestao-de-pessoas-dataset.js

--- portfolio/formacoes-complementares/gestao-de-pessoas/gestao-de-pessoas-dataset.js
+++ portfolio/formacoes-complementares/gestao-de-pessoas/gestao-de-pessoas-dataset.js
@@ -2,213 +2,303 @@ window.datasetCategoria = [
   {
     id: 9,
 
-    instituicao: "FIAP - Centro Universitário",
- 
-    curso: "Customer Experience Management",
-    
-    cargaHoraria: "40 h",
-
-    conclusaoISO: "2026-02-12",
-
-    dataConclusao: "12 de fevereiro de 2025",
-
-    codigo: "80222c2ebda653495e99505d9d68e992",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2026.02.12 · FIAP - Centro Universitário · Customer Experience Management.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "FIAP - Centro Universitário",
+    curso: "Customer Experience Management",
+    cargaHoraria: "40 h",
+    conclusaoISO: "2026-02-12",
+    dataConclusao: "12 de fevereiro de 2025",
+    codigo: "80222c2ebda653495e99505d9d68e992",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=80222c2ebda653495e99505d9d68e992&action=view",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 8,
 
-    instituicao: "Qulture Rocks",
- 
-    curso: "Certificação em Gestão de Desempenho para RH",
-    
-    cargaHoraria: "8 h",
-
-    conclusaoISO: "2025-10-21",
-
-    dataConclusao: "21 de outubro de 2025",
-
-    codigo: "35783_xQCURbnC13iAzayzCZVW_6128412",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.10.21 · Qulture Rocks · Gestão de Desempenho para RH.png",
 
-    verificacao: null
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Qulture Rocks",
+    curso: "Certificação em Gestão de Desempenho para RH",
+    cargaHoraria: "8 h",
+    conclusaoISO: "2025-10-21",
+    dataConclusao: "21 de outubro de 2025",
+    codigo: "35783_xQCURbnC13iAzayzCZVW_6128412",
+    mostrarCopiar: false,
+    verificacao: null,
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 7,
 
-    instituicao: "HP Foundation",
- 
-    curso: "Gestão de Relacionamento com Clientes",
-    
-    cargaHoraria: "1 h",
-
-    conclusaoISO: "2025-10-06",
-
-    dataConclusao: "06 de outubro de 2025",
-
-    codigo: "6b794d0d-9d74-4a2a-ae35-d091160c0ccf",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.10.06 · HP Foundation · Gestão de Relacionamento com Clientes.png",
 
-    verificacao: null
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "HP Foundation",
+    curso: "Gestão de Relacionamento com Clientes",
+    cargaHoraria: "1 h",
+    conclusaoISO: "2025-10-06",
+    dataConclusao: "06 de outubro de 2025",
+    codigo: "6b794d0d-9d74-4a2a-ae35-d091160c0ccf",
+    mostrarCopiar: false,
+    verificacao: null,
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 6,
 
-    instituicao: "HP Foundation",
- 
-    curso: "Experiência do Cliente para o Sucesso dos Negócios",
-    
-    cargaHoraria: "1 h",
-
-    conclusaoISO: "2025-10-06",
-
-    dataConclusao: "06 de outubro de 2025",
-
-    codigo: "669ae90d-b215-4cec-9c3e-a4c3b362f908",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.10.06 · HP Foundation · Experiência do Cliente para Sucesso dos Negócios.png",
 
-    verificacao: null
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "HP Foundation",
+    curso: "Experiência do Cliente para o Sucesso dos Negócios",
+    cargaHoraria: "1 h",
+    conclusaoISO: "2025-10-06",
+    dataConclusao: "06 de outubro de 2025",
+    codigo: "669ae90d-b215-4cec-9c3e-a4c3b362f908",
+    mostrarCopiar: false,
+    verificacao: null,
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 5,
 
-    instituicao: "FIAP - Centro Universitário",
- 
-    curso: "Jovens Líderes",
-    
-    cargaHoraria: "40 h",
-
-    conclusaoISO: "2025-08-28",
-
-    dataConclusao: "28 de agosto de 2025",
-
-    codigo: "982083a40722b474b11399c33a5cc012",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.08.28 · FIAP - Centro Universitário · Jovens Líderes.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "FIAP - Centro Universitário",
+    curso: "Jovens Líderes",
+    cargaHoraria: "40 h",
+    conclusaoISO: "2025-08-28",
+    dataConclusao: "28 de agosto de 2025",
+    codigo: "982083a40722b474b11399c33a5cc012",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=982083a40722b474b11399c33a5cc012&action=view",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 4,
 
-    instituicao: "Qulture Rocks",
- 
-    curso: "Certificação em T&D: da Gestão de Desempenho à Universidade Corporativa",
-    
-    cargaHoraria: "6 h",
-
-    conclusaoISO: "2025-08-20",
-
-    dataConclusao: "20 de agosto de 2025",
-
-    codigo: "35783_S50br3BexedAUqnUgZkL_6128412",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.08.20 · Qulture Rocks · Da Gestão de Desempenho à Universidade Corporativa.png",
 
-    verificacao: null
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Qulture Rocks",
+    curso: "Certificação em T&D: da Gestão de Desempenho à Universidade Corporativa",
+    cargaHoraria: "6 h",
+    conclusaoISO: "2025-08-20",
+    dataConclusao: "20 de agosto de 2025",
+    codigo: "35783_S50br3BexedAUqnUgZkL_6128412",
+    mostrarCopiar: false,
+    verificacao: null,
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 3,
 
-    instituicao: "FIAP - Centro Universitário",
- 
-    curso: "Leadership Communication",
-    
-    cargaHoraria: "40 h",
-
-    conclusaoISO: "2025-04-18",
-
-    dataConclusao: "18 de abril de 2025",
-
-    codigo: "a007b5fe21db52f7a94cc3f344b5bb7a",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.04.18 · FIAP - Centro Universitário · Leadership Communication.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "FIAP - Centro Universitário",
+    curso: "Leadership Communication",
+    cargaHoraria: "40 h",
+    conclusaoISO: "2025-04-18",
+    dataConclusao: "18 de abril de 2025",
+    codigo: "a007b5fe21db52f7a94cc3f344b5bb7a",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=a007b5fe21db52f7a94cc3f344b5bb7a&action=view",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 2,
 
-    instituicao: "FIAP - Centro Universitário",
- 
-    curso: "Customer Experience Management",
-    
-    cargaHoraria: "40 h",
-
-    conclusaoISO: "2025-04-17",
-
-    dataConclusao: "17 de abril de 2025",
-
-    codigo: "c032b68e8ca24c3f0664eb795bbaddc3",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2025.04.17 · FIAP - Centro Universitário · Customer Experience Management.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "FIAP - Centro Universitário",
+    curso: "Customer Experience Management",
+    cargaHoraria: "40 h",
+    conclusaoISO: "2025-04-17",
+    dataConclusao: "17 de abril de 2025",
+    codigo: "c032b68e8ca24c3f0664eb795bbaddc3",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=c032b68e8ca24c3f0664eb795bbaddc3&action=view",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 1,
 
-    instituicao: "FM2S Educação e Consultoria",
- 
-    curso: "Fundamentos da Gestão e Liderança",
-    
-    cargaHoraria: "5 h",
-
-    conclusaoISO: "2024-10-14",
-
-    dataConclusao: "14 de outubro de 2024",
-
-    codigo: "Y2VydGlmaWNhdGVfNDI3MDIyXzg5NQ%3D%3D",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-de-pessoas/certificados/2024.10.14 · FM2S · Fundamentos da Gestão e Liderança.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "FM2S Educação e Consultoria",
+    curso: "Fundamentos da Gestão e Liderança",
+    cargaHoraria: "5 h",
+    conclusaoISO: "2024-10-14",
+    dataConclusao: "14 de outubro de 2024",
+    codigo: "Y2VydGlmaWNhdGVfNDI3MDIyXzg5NQ%3D%3D",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://ead.fm2s.com.br/verify/Y2VydGlmaWNhdGVfNDI3MDIyXzg5NQ%3D%3D",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   }
 ];
