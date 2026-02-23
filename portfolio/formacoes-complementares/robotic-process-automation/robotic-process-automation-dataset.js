a27443404d39763b5b867bee867ee4c8e13b9ebe
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-23 16:27:12 -0300

    Update robotic-process-automation-dataset.js

--- portfolio/formacoes-complementares/robotic-process-automation/robotic-process-automation-dataset.js
+++ portfolio/formacoes-complementares/robotic-process-automation/robotic-process-automation-dataset.js
@@ -2,22 +2,32 @@ window.datasetCategoria = [
   {
     id: 1,
 
-    instituicao: "RPA Solutions",
- 
-    curso: "Robotic Process Automation",
-    
-    cargaHoraria: "8 h",
-
-    conclusaoISO: "2024-02-17",
-
-    dataConclusao: "17 de fevereiro de 2024",
-
-    codigo: "Inaplicável",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/robotic-process-automation/certificados/2024.02.17 · RPA Solutions · Robotic Process Automation.png",
 
-    verificacao: null
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "RPA Solutions",
+    curso: "Robotic Process Automation",
+    cargaHoraria: "8 h",
+    conclusaoISO: "2024-02-17",
+    dataConclusao: "17 de fevereiro de 2024",
+    codigo: "Inaplicável",
+    mostrarCopiar: false,
+    verificacao: null,
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   }
 ];
