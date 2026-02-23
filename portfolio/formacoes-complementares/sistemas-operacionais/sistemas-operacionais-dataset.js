5dbaca088e16a8e8aef45c6ccc280ec0749a9c24
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-23 16:34:06 -0300

    Update sistemas-operacionais-dataset.js

--- portfolio/formacoes-complementares/sistemas-operacionais/sistemas-operacionais-dataset.js
+++ portfolio/formacoes-complementares/sistemas-operacionais/sistemas-operacionais-dataset.js
@@ -2,22 +2,22 @@ window.datasetCategoria = [
   {
     id: 6,
 
-    instituicao: "FIAP - Centro Universitário",
- 
-    curso: "Android",
-    
-    cargaHoraria: "80 h",
-
-    conclusaoISO: "2026-02-07",
-
-    dataConclusao: "07 de fevereiro de 2026",
-
-    codigo: "a6a545c534df69178affcc13e068f2ef",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2026.02.07 · FIAP - Centro Universitário · Android.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "FIAP - Centro Universitário",
+    curso: "Android",
+    cargaHoraria: "80 h",
+    conclusaoISO: "2026-02-07",
+    dataConclusao: "07 de fevereiro de 2026",
+    codigo: "a6a545c534df69178affcc13e068f2ef",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=a6a545c534df69178affcc13e068f2ef&action=view",
       texto: "Verificar Autenticidade"
@@ -27,22 +27,22 @@ window.datasetCategoria = [
   {
     id: 5,
 
-    instituicao: "Cursa",
- 
-    curso: "Máquinas Virtuais e Instalação de Sistemas",
-    
-    cargaHoraria: "9 h",
-
-    conclusaoISO: "2025-10-15",
-
-    dataConclusao: "15 de outubro de 2025",
-
-    codigo: "727ec39241",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.10.15 · Cursa · Máquinas Virtuais e Instalação de Sistemas.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Cursa",
+    curso: "Máquinas Virtuais e Instalação de Sistemas",
+    cargaHoraria: "9 h",
+    conclusaoISO: "2025-10-15",
+    dataConclusao: "15 de outubro de 2025",
+    codigo: "727ec39241",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://cursa.com.br/certificate/727ec39241",
       texto: "Verificar Autenticidade"
@@ -52,22 +52,22 @@ window.datasetCategoria = [
   {
     id: 4,
 
-    instituicao: "Instituto Federal Rio Grande do Sul",
- 
-    curso: "Windows Server 2016: Active Directory",
-    
-    cargaHoraria: "40 h",
-
-    conclusaoISO: "2025-10-06",
-
-    dataConclusao: "06 de outubro de 2025",
-
-    codigo: "68e41d95-ad74-4f81-a1d6-cec00aa8150d",
-
-    mostrarCopiar: true,
-
     thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.10.06 · Instituto Federal Rio Grande do Sul · Windows Server 2016 (Active Directory).png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Instituto Federal Rio Grande do Sul",
+    curso: "Windows Server 2016: Active Directory",
+    cargaHoraria: "40 h",
+    conclusaoISO: "2025-10-06",
+    dataConclusao: "06 de outubro de 2025",
+    codigo: "68e41d95-ad74-4f81-a1d6-cec00aa8150d",
+    mostrarCopiar: true,
     verificacao: {
       url: "https://moodle.ifrs.edu.br/mod/simplecertificate/verify.php",
       texto: "Verificar Autenticidade"
@@ -77,22 +77,22 @@ window.datasetCategoria = [
   {
     id: 3,
 
-    instituicao: "Instituto Federal Rio Grande do Sul",
- 
-    curso: "Windows Server 2016: Instalação e Administração",
-    
-    cargaHoraria: "40 h",
-
-    conclusaoISO: "2025-10-06",
-
-    dataConclusao: "06 de outubro de 2025",
-
-    codigo: "68e41cf2-4018-4b7a-b928-e2810aa80106",
-
-    mostrarCopiar: true,
-
     thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.10.06 · Instituto Federal Rio Grande do Sul · Windows Server 2016 (Instalação e Administração).png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Instituto Federal Rio Grande do Sul",
+    curso: "Windows Server 2016: Instalação e Administração",
+    cargaHoraria: "40 h",
+    conclusaoISO: "2025-10-06",
+    dataConclusao: "06 de outubro de 2025",
+    codigo: "68e41cf2-4018-4b7a-b928-e2810aa80106",
+    mostrarCopiar: true,
     verificacao: {
       url: "https://moodle.ifrs.edu.br/mod/simplecertificate/verify.php",
       texto: "Verificar Autenticidade"
@@ -102,22 +102,22 @@ window.datasetCategoria = [
   {
     id: 2,
 
-    instituicao: "FIAP - Centro Universitário",
- 
-    curso: "Android",
-    
-    cargaHoraria: "80 h",
-
-    conclusaoISO: "2025-04-15",
-
-    dataConclusao: "15 de abril de 2025",
-
-    codigo: "ff03a7806ef30659780b9ff061b98936",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.04.15 · FIAP - Centro Universitário · Android.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "FIAP - Centro Universitário",
+    curso: "Android",
+    cargaHoraria: "80 h",
+    conclusaoISO: "2025-04-15",
+    dataConclusao: "15 de abril de 2025",
+    codigo: "ff03a7806ef30659780b9ff061b98936",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=ff03a7806ef30659780b9ff061b98936&action=view",
       texto: "Verificar Autenticidade"
@@ -127,22 +127,22 @@ window.datasetCategoria = [
   {
     id: 1,
 
-    instituicao: "FIAP - Centro Universitário",
- 
-    curso: "Linux Fundamentos",
-    
-    cargaHoraria: "40 h",
-
-    conclusaoISO: "2025-03-10",
-
-    dataConclusao: "10 de março de 2025",
-
-    codigo: "b680d2fffda89a1e7ad608953a3f7fac",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/sistemas-operacionais/certificados/2025.03.10 · FIAP Centro Universitário · Linux Fundamentos.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "FIAP - Centro Universitário",
+    curso: "Linux Fundamentos",
+    cargaHoraria: "40 h",
+    conclusaoISO: "2025-03-10",
+    dataConclusao: "10 de março de 2025",
+    codigo: "b680d2fffda89a1e7ad608953a3f7fac",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=b680d2fffda89a1e7ad608953a3f7fac&action=view",
       texto: "Verificar Autenticidade"
