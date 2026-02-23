5616b84a3dfbf9911ef1b137773d9eec59e8fc88
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-23 16:36:05 -0300

    Update skills-dataset.js

--- portfolio/formacoes-complementares/skills/skills-dataset.js
+++ portfolio/formacoes-complementares/skills/skills-dataset.js
@@ -2,47 +2,67 @@ window.datasetCategoria = [
   {
     id: 2,
 
-    instituicao: "VMEdu, Inc",
- 
-    curso: "Negotiation Associate",
-    
-    cargaHoraria: "8 h",
-
-    conclusaoISO: "2025-11-15",
-
-    dataConclusao: "15 de novembro de 2025",
-
-    codigo: "1126711",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/skills/certificados/2025.11.15 · VMEdu, Inc · Negotiation Associate.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "VMEdu, Inc",
+    curso: "Negotiation Associate",
+    cargaHoraria: "8 h",
+    conclusaoISO: "2025-11-15",
+    dataConclusao: "15 de novembro de 2025",
+    codigo: "1126711",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/NegotiationAssociate-RogérioClyntonRibeiro-1126711.pdf",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
 
   {
     id: 1,
 
-    instituicao: "IBM SkillsBuild",
- 
-    curso: "Faça por Merecer! Aceite! Compartilhe! | IBM SkillsBuild",
-    
-    cargaHoraria: "20 min",
-
-    conclusaoISO: "2025-10-21",
-
-    dataConclusao: "21 de outubro de 2025",
-
-    codigo: "URL-CC7432BB7A8A",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/skills/certificados/2025.10.21 · IBM SkillsBuild · Faça por Merecer! Aceite! Compartilhe! IBM SkillsBuild.png",
 
-    verificacao: null
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "IBM SkillsBuild",
+    curso: "Faça por Merecer! Aceite! Compartilhe! | IBM SkillsBuild",
+    cargaHoraria: "20 min",
+    conclusaoISO: "2025-10-21",
+    dataConclusao: "21 de outubro de 2025",
+    codigo: "URL-CC7432BB7A8A",
+    mostrarCopiar: false,
+    verificacao: null,
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   }
 ];
