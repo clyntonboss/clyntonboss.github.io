d33e6189ac41056227809f5aa26476c183076ab3
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-23 00:30:27 -0300

    Update gestao-financeira-dataset.js

--- portfolio/formacoes-complementares/gestao-financeira/gestao-financeira-dataset.js
+++ portfolio/formacoes-complementares/gestao-financeira/gestao-financeira-dataset.js
@@ -2,50 +2,70 @@ window.datasetCategoria = [
   {
     id: 2,
 
-    instituicao: "FIAP - Centro Universitário",
- 
-    curso: "Gestão Financeira de Empresas",
-    
-    cargaHoraria: "20 h",
-
-    conclusaoISO: "2026-02-18",
-
-    dataConclusao: "18 de fevereiro de 2026",
-
-    codigo: "c728d0c905786e7be875d51463eadff8",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-financeira/certificados/2026.02.18 · FIAP - Centro Universitário · Gestão Financeira de Empresas.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "FIAP - Centro Universitário",
+    curso: "Gestão Financeira de Empresas",
+    cargaHoraria: "20 h",
+    conclusaoISO: "2026-02-18",
+    dataConclusao: "18 de fevereiro de 2026",
+    codigo: "c728d0c905786e7be875d51463eadff8",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=c728d0c905786e7be875d51463eadff8&action=view",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 1,
 
-    instituicao: "FIAP - Centro Universitário",
- 
-    curso: "Gestão Financeira de Empresas",
-    
-    cargaHoraria: "20 h",
-
-    conclusaoISO: "2025-04-21",
-
-    dataConclusao: "21 de abril de 2025",
-
-    codigo: "6c8a5531a603a83b7cdc386476bf1c14",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/gestao-financeira/certificados/2025.04.21 · FIAP - Centro Universitário · Gestão Financeira de Empresas.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "FIAP - Centro Universitário",
+    curso: "Gestão Financeira de Empresas",
+    cargaHoraria: "20 h",
+    conclusaoISO: "2025-04-21",
+    dataConclusao: "21 de abril de 2025",
+    codigo: "6c8a5531a603a83b7cdc386476bf1c14",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://on.fiap.com.br/local/nanocourses/gerar_certificado.php?chave=6c8a5531a603a83b7cdc386476bf1c14&action=view",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   }
 ];
