d368318fdc95a5025b034f0d057622bf690c8499
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-23 00:36:06 -0300

    Update idiomas-dataset.js

--- portfolio/formacoes-complementares/idiomas/idiomas-dataset.js
+++ portfolio/formacoes-complementares/idiomas/idiomas-dataset.js
@@ -2,97 +2,137 @@ window.datasetCategoria = [
   {
     id: 4,
 
-    instituicao: "Hashtag Treinamentos",
- 
-    curso: "Jornada Inglês da Hashtag",
-    
-    cargaHoraria: "8 h",
-
-    conclusaoISO: "2025-07-17",
-
-    dataConclusao: "17 de julho de 2025",
-
-    codigo: "Inaplicável",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/idiomas/certificados/2025.07.17 · Hashtag Treinamentos · Jornada Inglês da Hashtag.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Hashtag Treinamentos",
+    curso: "Jornada Inglês da Hashtag",
+    cargaHoraria: "8 h",
+    conclusaoISO: "2025-07-17",
+    dataConclusao: "17 de julho de 2025",
+    codigo: "Inaplicável",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://dlp.hashtagtreinamentos.com/certificado/download?ncf=Rogério+Clynton+Ribeiro&necf=Jornada+Inglês+da+Hashtag&dtcf=17%2F07%2F2025&hcf=8",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 3,
 
-    instituicao: "Hashtag Treinamentos",
- 
-    curso: "Jornada Inglês da Hashtag",
-    
-    cargaHoraria: "8 h",
-
-    conclusaoISO: "2025-01-09",
-
-    dataConclusao: "09 de janeiro de 2025",
-
-    codigo: "Inaplicável",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/idiomas/certificados/2025.01.09 · Hashtag Treinamentos · Jornada Inglês da Hashtag.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Hashtag Treinamentos",
+    curso: "Jornada Inglês da Hashtag",
+    cargaHoraria: "8 h",
+    conclusaoISO: "2025-01-09",
+    dataConclusao: "09 de janeiro de 2025",
+    codigo: "Inaplicável",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://dlp.hashtagtreinamentos.com/certificado/download?ncf=Rogério+Clynton+Ribeiro&necf=Jornada+Inglês+da+Hashtag&dtcf=09%2F01%2F2025&hcf=8",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 2,
 
-    instituicao: "Hashtag Treinamentos",
- 
-    curso: "Jornada Inglês da Hashtag",
-    
-    cargaHoraria: "8 h",
-
-    conclusaoISO: "2024-10-03",
-
-    dataConclusao: "03 de outubro de 2024",
-
-    codigo: "Inaplicável",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/idiomas/certificados/2024.10.03 · Hashtag Treinamentos · Jornada Inglês da Hashtag.png",
 
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Hashtag Treinamentos",
+    curso: "Jornada Inglês da Hashtag",
+    cargaHoraria: "8 h",
+    conclusaoISO: "2024-10-03",
+    dataConclusao: "03 de outubro de 2024",
+    codigo: "Inaplicável",
+    mostrarCopiar: false,
     verificacao: {
       url: "https://dlp.hashtagtreinamentos.com/certificado/download?ncf=Rogério+Clynton+Ribeiro&necf=Jornada+Inglês+da+Hashtag&dtcf=03%2F10%2F2024&hcf=8",
       texto: "Verificar Autenticidade"
-    }
+    },
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   },
   
   {
     id: 1,
 
-    instituicao: "Fluênciassim",
- 
-    curso: "Imersão em Inglês",
-    
-    cargaHoraria: "8 h",
-
-    conclusaoISO: "2024-03-10",
-
-    dataConclusao: "10 de março de 2024",
-
-    codigo: "Inaplicável",
-
-    mostrarCopiar: false,
-
     thumb: "/portfolio/formacoes-complementares/idiomas/certificados/2024.03.10 · Fluênciassim · Imersão em Inglês.png",
 
-    verificacao: null
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Fluênciassim",
+    curso: "Imersão em Inglês",
+    cargaHoraria: "8 h",
+    conclusaoISO: "2024-03-10",
+    dataConclusao: "10 de março de 2024",
+    codigo: "Inaplicável",
+    mostrarCopiar: false,
+    verificacao: null,
+
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
   }
 ];
