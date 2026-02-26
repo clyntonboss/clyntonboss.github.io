dcda6c1076b2506b2f5faafaeb38259a91fa77c5
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-02-26 15:03:04 -0300

    Update portfolio-sections.js

--- portfolio/portfolio-sections.js
+++ portfolio/portfolio-sections.js
@@ -2690,7 +2690,7 @@ const sections = {
               <span class="exp-role">EaD: a nova onda da educação - Virtudes e Desafios</span>
               <button class="exp-toggle">▸</button>
           </div>
-          <div class="exp-details exp-details-artigos">
+          <div class="exp-details">
       
               <hr class="decorative-line-cert decorative-line-sup">
       
