cc76fb6038afc9215228905f669761b37c190099
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-03-04 10:37:09 -0300

    Update logistica-dataset.js

--- portfolio/projetos/microsoft-power-bi/logistica/logistica-dataset.js
+++ portfolio/projetos/microsoft-power-bi/logistica/logistica-dataset.js
@@ -75,8 +75,8 @@ window.datasetCategoria = [
     nomeStackTecnica: "Stack Técnica: ",
   
     titulo: "Dashboard de Gestão Logística",
-    periodo: "Jan 2026",
-    duracao: "12 dias",
+    periodo: "Jan 2022",
+    duracao: "16 dias",
     stackTecnica: "Power BI | DAX | Modelagem Dimensional",
     projetoInterativo: `<a href="https://app.powerbi.com/view?r=eyJrIjoiMWVhZGU1MjctYjVhMS00MjJlLWFiMWItZjk1OGJhODY5MjgzIiwidCI6IjIzY2FjN2VlLWYxZDgtNDMzOS1hYTdiLTc4MWFhOWY5MjI1YiJ9" target="_blank" class="cert-link-verify cert-link-inf">Projeto Interativo</a>`,
     descricaoProjeto: `
