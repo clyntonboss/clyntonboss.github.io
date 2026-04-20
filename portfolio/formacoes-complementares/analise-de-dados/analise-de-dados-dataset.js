98cdd5030751de2c02a3b327a1d1b6594d0acc4c
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-04-20 14:50:19 -0300

    Update analise-de-dados-dataset.js

--- portfolio/formacoes-complementares/analise-de-dados/analise-de-dados-dataset.js
+++ portfolio/formacoes-complementares/analise-de-dados/analise-de-dados-dataset.js
@@ -49,6 +49,43 @@
 
 window.datasetCategoria = [
   {
+    id: 309,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2026.04.11 · Digital Innovation One · Construindo um Esquema Conceitual para Banco de Dados.png",
+
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Digital Innovation One",
+    curso: "Construindo um Esquema Conceitual para Banco de Dados",
+    cargaHoraria: "1 h",
+    conclusaoISO: "2026-04-11",
+    dataConclusao: "11 de abril de 2026",
+    codigo: "8TIR8GCX",
+    mostrarCopiar: false,
+    verificacao: {
+      url: "https://hermes.dio.me/certificates/8TIR8GCX.pdf",
+      texto: "Verificar Autenticidade"
+    },
+
+    nomeTitulo: "",
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    titulo: "",
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
+  },
+
+  {
     id: 308,
 
     thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2026.04.11 · Digital Innovation One · Refinando um Projeto Conceitual de Banco de Dados - e-Commerce.png",
