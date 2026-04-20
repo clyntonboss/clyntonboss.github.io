709328fc3250fa361e871908dba79fa1e488a260
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-04-20 13:05:29 -0300

    Update analise-de-dados-dataset.js

--- portfolio/formacoes-complementares/analise-de-dados/analise-de-dados-dataset.js
+++ portfolio/formacoes-complementares/analise-de-dados/analise-de-dados-dataset.js
@@ -49,6 +49,43 @@
 
 window.datasetCategoria = [
   {
+    id: 301,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2026.04.11 · Digital Innovation One · Modelagem de Dados para Banco de Dados.png",
+
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Digital Innovation One",
+    curso: "Modelagem de Dados para Banco de Dados",
+    cargaHoraria: "2 h",
+    conclusaoISO: "2026-04-11",
+    dataConclusao: "11 de abril de 2026",
+    codigo: "JXS6NFKV",
+    mostrarCopiar: false,
+    verificacao: {
+      url: "https://hermes.dio.me/certificates/JXS6NFKV.pdf",
+      texto: "Verificar Autenticidade"
+    },
+
+    nomeTitulo: "",
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    titulo: "",
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
+  },
+
+  {
     id: 300,
 
     thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2026.04.11 · Digital Innovation One · Sistemas de Gerenciamento de Banco de Dados.png",
