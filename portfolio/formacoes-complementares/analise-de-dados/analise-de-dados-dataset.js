c91d577af66711289d833681686693ad1823856f
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-04-20 13:59:49 -0300

    Update analise-de-dados-dataset.js

--- portfolio/formacoes-complementares/analise-de-dados/analise-de-dados-dataset.js
+++ portfolio/formacoes-complementares/analise-de-dados/analise-de-dados-dataset.js
@@ -49,6 +49,43 @@
 
 window.datasetCategoria = [
   {
+    id: 305,
+
+    thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2026.04.11 · Digital Innovation One · Fundamentos de Modelagem e Projeto de Banco de Dados.png",
+
+    nomeInstituicao: "Instituição: ",
+    nomeCurso: "Curso: ",
+    nomeCargaHoraria: "Carga Horária: ",
+    nomeDataConclusao: "Data de Conclusão: ",
+    nomeCodigo: "Código: ",
+    nomeVerificacao: "Verificação: ",
+
+    instituicao: "Digital Innovation One",
+    curso: "Fundamentos de Modelagem e Projeto de Banco de Dados",
+    cargaHoraria: "3 h",
+    conclusaoISO: "2026-04-11",
+    dataConclusao: "11 de abril de 2026",
+    codigo: "UQ6BEKEB",
+    mostrarCopiar: false,
+    verificacao: {
+      url: "https://hermes.dio.me/certificates/UQ6BEKEB.pdf",
+      texto: "Verificar Autenticidade"
+    },
+
+    nomeTitulo: "",
+    nomePeriodo: "",
+    nomeDuracao: "",
+    nomeStackTecnica: "",
+
+    titulo: "",
+    periodo: "",
+    duracao: "",
+    stackTecnica: "",
+    projetoInterativo: "",
+    descricaoProjeto: ""
+  },
+
+  {
     id: 304,
 
     thumb: "/portfolio/formacoes-complementares/analise-de-dados/certificados/2026.04.11 · Digital Innovation One · Live de Lançamento - Coding the Future Suzano - Introdução a Banco de Dados Relacionais.png",
