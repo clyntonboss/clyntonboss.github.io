1bd572e6305f7a972fc10ef8ddd616dfbe818569
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-04-30 15:35:35 -0300

    Update course name formatting in dataset

--- portfolio/formacoes-complementares/inteligencia-artificial/inteligencia-artificial-dataset.js
+++ portfolio/formacoes-complementares/inteligencia-artificial/inteligencia-artificial-dataset.js
@@ -764,7 +764,7 @@ window.datasetCategoria = [
     nomeVerificacao: "Verificação: ",
 
     instituicao: "Digital Innovation One",
-    curso: "Materiais Complementares (Frameworks de Deep Learning)",
+    curso: "Materiais Complementares: Frameworks de Deep Learning",
     cargaHoraria: "1 h",
     conclusaoISO: "2026-04-15",
     dataConclusao: "15 de abril de 2026",
