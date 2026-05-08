0cb8a74da9ac53f342d3ae80caa39d24d6e322b8
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2026-05-08 18:47:52 -0300

    Fix course title formatting in dataset

--- portfolio/formacoes-complementares/inteligencia-artificial/inteligencia-artificial-dataset.js
+++ portfolio/formacoes-complementares/inteligencia-artificial/inteligencia-artificial-dataset.js
@@ -209,7 +209,7 @@ window.datasetCategoria = [
     nomeVerificacao: "Verificação: ",
 
     instituicao: "Digital Innovation One",
-    curso: "IA Entrevistador (Seu Simulador Inteligente de Entrevistas)",
+    curso: "IA Entrevistador: Seu Simulador Inteligente de Entrevistas",
     cargaHoraria: "2 h",
     conclusaoISO: "2026-04-22",
     dataConclusao: "22 de abril de 2026",
